dda11e418f04e2ada62e20ef5361f5b058d0eafd
Martin Hoegh <mhoegh@gmail.com>
2011-07-11 10:29:44 +0200

    Added support for query buffer in viewer

--- controller/tables/tables_c.php
+++ controller/tables/tables_c.php
@@ -16,9 +16,11 @@ switch ($parts[4]){
 	break;
 	case "updaterecord": // All tables
 		$response = $table -> updateRecord($obj->data,$parts[6]);
+		makeMapFile($_SESSION['screen_name']);
 	break;
 	case "destroyrecord": // Geometry columns
 		$response = $table -> destroyRecord($obj->data,$parts[6]);
+		makeMapFile($_SESSION['screen_name']);
 	break;
 	case 'getcolumns': // All tables
 		$response = $table -> getColumnsForExtGridAndStore();
@@ -28,15 +30,19 @@ switch ($parts[4]){
 	break;
 	case 'updatecolumn':
 		$response = $table -> updateColumn($obj->data);
+		makeMapFile($_SESSION['screen_name']);
 		break;
 	case 'createcolumn':
 		$response = $table -> addColumn($_POST); // Is POSTED by a form
+		makeMapFile($_SESSION['screen_name']);
 		break;
 	case 'destroycolumn':
 		$response = $table -> deleteColumn($obj->data);
+		makeMapFile($_SESSION['screen_name']);
 		break;
 	case 'addcolumn':
 		$response = $table -> addColumn($obj->data);
+		makeMapFile($_SESSION['screen_name']);
 		break;
 }
 include_once("../server_footer.inc");
