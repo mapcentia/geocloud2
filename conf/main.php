9bfc948084daa2da37423591e4be5f1c723363b3
Martin Hoegh <mhoegh@gmail.com>
2012-01-09 09:46:28 +0100

    Some fixes

--- conf/main.php
+++ conf/main.php
@@ -2,24 +2,26 @@
 ini_set("display_errors", "On");
 error_reporting(3);
 
+// URL and path information. Must be the web root folder! 
 $hostName = "http://beta.mygeocloud.com";
-
-//$basePath = $_SERVER["DOCUMENT_ROOT"]."/";
 $basePath = "/var/www/mygeocloud/";
-set_include_path(get_include_path() . PATH_SEPARATOR . $basePath . PATH_SEPARATOR . $basePath."libs" . PATH_SEPARATOR . $basePath."inc" . PATH_SEPARATOR . $basePath."libs/PEAR/" . PATH_SEPARATOR . $basePath."conf");
 
 
-// PostGIS connection
+// PostGreSQL connection
 if (!$postgishost) $postgishost="127.0.0.1";
 if (!$postgisdb) $postgisdb="postgres";
 if (!$postgisuser) $postgisuser="postgres";
 if (!$postgisport) $postgisport="";
 if (!$postgispw) $postgispw="";
 
+// Database template for creating new databases
 $databaseTemplate = "template_mygeocloud";
 
+// Use PostGIS or PHP to export GML
 $useWktToGmlInPHP = false;
 
+// Your Google Maps API key
 $gMapsApiKey = "ABQIAAAAixUaqWcOE1cqF2LJyDYCdhS4p9AtMz66nyqFUaziGHLM44rOahQ1vHhpXeGXl_ifkSE8O1eT_foV2w";
 
-$cacheDir = $basePath."tmp/cache/";
+// Include path setting. You may not need to alter this
+set_include_path(get_include_path() . PATH_SEPARATOR . $basePath . PATH_SEPARATOR . $basePath."libs" . PATH_SEPARATOR . $basePath."inc" . PATH_SEPARATOR . $basePath."libs/PEAR/" . PATH_SEPARATOR . $basePath."conf");
--- html_header.php
+++ html_header.php
@@ -31,15 +31,14 @@ if ($parts[1]=="store" || $parts[1]=="editor") {
 	<script type="text/javascript">
 
 	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-24038130-1']);
-	  _gaq.push(['_setDomainName', '.mygeocloud.com']);
+	  _gaq.push(['_setAccount', 'UA-28178450-1']);
+	  _gaq.push(['_setDomainName', 'mygeocloud.com']);
 	  _gaq.push(['_trackPageview']);
-
+	
 	  (function() {
 	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  })();
-
+	
 	</script>
-		
