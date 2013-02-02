c3f04b0d8b8031c45c13fddacb29242dcd6397e1
Martin Hoegh <mhoegh@gmail.com>
2013-02-02 23:59:33 +0100

    user stuff

---
+++ user/header.php
@@ -0,0 +1,10 @@
+<?php
+include '../../conf/main.php';
+session_set_cookie_params(0, '/',$domain);
+session_start();
+include '../header.html';
+include '../../libs/functions.php';
+include '../../model/settings_viewer.php';
+include 'model/databases.php';
+$postgisdb = 'mygeocloud';
+$sTable = 'users';
