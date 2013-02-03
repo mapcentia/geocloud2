1a57a8db6b4f5414cfe7893bb1d0db5a43feff6f
Martin Hoegh <mhoegh@gmail.com>
2013-02-03 19:34:18 +0100

    h

--- user/logout/logout_f.php
+++ user/logout/logout_f.php
@@ -1,4 +1,7 @@
 <?php
+include 'conf/main.php';
+session_name($sessionName);
+session_set_cookie_params(0, '/',".".$domain);
 session_start();
 $_SESSION['auth'] = false;
 $_SESSION['screen_name'] = false;
