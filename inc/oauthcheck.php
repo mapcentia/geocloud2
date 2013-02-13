1fa61962caa83b1a7059098b3b79d8d0613ef226
Martin Hoegh <mhoegh@gmail.com>
2013-02-13 15:03:38 +0100

    redirect to user host

--- inc/oauthcheck.php
+++ inc/oauthcheck.php
@@ -3,5 +3,5 @@ $postgisdb = $parts[2];
 if (!$_SESSION['auth'] || ($_SESSION['screen_name'] != $parts[2])) {
 	$_SESSION['auth']=null;
 	$_SESSION['screen_name']=null;
-	die("<script>window.location='/user/login'</script>");
+	die("<script>window.location='{$userHostName}/user/login'</script>");
 }
