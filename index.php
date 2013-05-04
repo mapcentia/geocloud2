2833e5abae0d88bbb6e6cd9e8393519f6339c0b3
Martin Høgh <mh@mapcentia.com>
2013-05-04 21:39:21 +0200

    google analytics

--- index.php
+++ index.php
@@ -67,23 +67,6 @@ session_start();
         }
 
     </style>
-
-    <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-28178450-1']);
-        _gaq.push(['_setDomainName', 'mygeocloud.com']);
-        _gaq.push(['_trackPageview']);
-
-        (function () {
-            var ga = document.createElement('script');
-            ga.type = 'text/javascript';
-            ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(ga, s);
-        })();
-
-    </script>
 </head>
 <body>
     <?php include_once("analyticstracking.php") ?>
