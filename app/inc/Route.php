2c4f05e9c64be4c64364d9c0401744140f4b2ac8
Martin Høgh <mh@mapcentia.com>
2013-11-04 12:27:10 +0100

    Bug

--- app/inc/Route.php
+++ app/inc/Route.php
@@ -14,10 +14,12 @@ class Route
                 $func();
             }
             $uri = rtrim($uri, "/");
-            $n = sizeof(explode("/", $uri));
+            $e = explode("/", $uri);
+            $e[count($e)-1] = ucfirst($e[count($e)-1]);
+            $uri = implode($e, "/");
+            $n = sizeof($e);
             $className = strtr($uri, '/', '\\');
             $class = "app\\{$className}";
-
             $action = Input::getMethod() . "_" . Input::getPath()->part($n + 1);
             if (class_exists($class)) {
                 $controller = new $class();
