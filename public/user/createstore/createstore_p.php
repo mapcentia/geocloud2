c5cfc5d01728fe592bab4fcc1b8cdb44599d4dc9
Martin Høgh <mh@mapcentia.com>
2013-11-30 01:00:01 +0100

    Redirect fixed

--- public/user/createstore/createstore_p.php
+++ public/user/createstore/createstore_p.php
@@ -9,14 +9,14 @@ if (!$_SESSION['screen_name']) {
 
 
 } else {
-	$name = Model::toAscii($_SESSION['screen_name'], NULL, "_");
-	$db = new \app\models\Database;
-	$dbObj = $db -> createdb($name, App::$param['databaseTemplate'], "UTF8");
-	// databaseTemplate is set in conf/main.php
-	if ($dbObj) {
-		header("location: {$userHostName}/user/login/p");
-	} else {
-		echo "<h2>Sorry, something went wrong. Try again</h2>";
-		echo "<div><a href='/user/signup' class='btn btn-danger'>Go back</a></div>";
-	}
+    $name = Model::toAscii($_SESSION['screen_name'], NULL, "_");
+    $db = new \app\models\Database;
+    $dbObj = $db->createdb($name, App::$param['databaseTemplate'], "UTF8");
+    // databaseTemplate is set in conf/main.php
+    if ($dbObj) {
+        header("location: " . \app\conf\App::$param['userHostName'] . "}/user/login/p");
+    } else {
+        echo "<h2>Sorry, something went wrong. Try again</h2>";
+        echo "<div><a href='" . \app\conf\App::$param['userHostName'] . "/user/signup' class='btn btn-danger'>Go back</a></div>";
+    }
 }
