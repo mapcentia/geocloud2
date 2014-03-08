6dd054cbd0ea64dfb1823d1f74f9372a29526c7a
Martin Høgh <mh@mapcentia.com>
2014-03-08 23:39:46 +0100

    path

--- public/api/v1/js/api.js
+++ public/api/v1/js/api.js
@@ -441,13 +441,12 @@ var mygeocloud_ol = (function () {
             }
             return layersArr;
         };
-
         this.createTileLayer = function (layer, defaults) {
             var parts = layer.split(".");
             if (!defaults.tileCached) {
                 var url = host + "/wms/" + this.db + "/" + parts[0] + "/?";
             } else {
-                var url = host + "/cgi/tilecache.py?cfg=" + this.db;
+                var url = host + "/wms/" + this.db + "/" + parts[0] + "/tilecache/";
             }
             var l = new OpenLayers.Layer.WMS(defaults.name, url, {
                 layers: layer,
