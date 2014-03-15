21724ea28b3c4fceaa9e9ef697f946495bd3d83b
Martin Høgh <mh@mapcentia.com>
2014-03-15 17:07:27 +0100

    bug

--- public/js/store.js
+++ public/js/store.js
@@ -54,7 +54,7 @@ $(window).ready(function () {
                 if (http.status === 200) {
                     settings = data.data;
                     $("#apikeyholder").html(settings.api_key);
-                    if (settings.extents[schema] !== undefined) {
+                    if (typeof settings.extents[schema] !== "undefined") {
                         initExtent = settings.extents[schema];
                     }
                 }
