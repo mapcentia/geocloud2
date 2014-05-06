1fcefce43709cdd5c916a946e65292ee4ac16145
Martin Høgh <mh@mapcentia.com>
2014-05-06 15:35:07 +0200

    migration script

--- app/inc/Model.php
+++ app/inc/Model.php
@@ -121,7 +121,7 @@ class Model
 
     function execQuery($query, $conn = "PDO", $queryType = "select")
     {
-        //echo($query)."\n";
+        $result = null;
         switch ($conn) {
             case "PG" :
                 if (!$this->db) {
