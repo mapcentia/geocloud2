79f40fa7426f75ed838f94a16581c4239530653d
Martin Høgh <mh@mapcentia.com>
2014-06-20 11:20:45 +0200

    Alert on widget

--- public/js/i18n/da_DK.js
+++ public/js/i18n/da_DK.js
@@ -7,6 +7,6 @@ gc2i18n = {
         "Pop up": "Stort kort",
         "Base layers": "Baggrund",
         "Share": "Del",
-        "Info text": "Vi har fået et nyt kort. Alle funktionerne er flyttet op i værktøjslinjen  øverst i kortet."
+        "Info text": "Vi har f&aring;et et nyt kort. Alle funktionerne er flyttet op i v&aelig;rkt&oslash;jslinjen &oslash;verst i kortet."
     }
 };
