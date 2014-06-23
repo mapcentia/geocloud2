17df2f8f08ba6f607c0742e6e10377f170f14c5c
amnesic <loic@justmagic.com>
2014-06-23 17:12:44 +0200

    French translation.

---
+++ public/js/i18n/fr_FR.js
@@ -0,0 +1,21 @@
+var gc2i18n;
+gc2i18n = {
+    dict: {
+        "Legend": "Légende",
+        "Property": "Propriété",
+        "Value": "Valeur",
+        "Pop up": "Pop up",
+        "Layers": "Couches",
+        "Baselayers": "Couches de base",
+        "Share": "Partager",
+        "Info": "Info",
+        "Close": "Fermer",
+        "Back": "Retour",
+        "Share the map": "Partager la carte",
+        "URL to this map": "URL de la carte",
+        "Search": "Rechercher",
+        "Iframe with this map to embed on web page": "Iframe de cette carte pour l'intégration à vos pages web",
+        "URL to a static PNG image of this map": "URL de l'image statique en PNG de cette carte",
+        "JavaScript for an application": "Intégration de cette carte via JavaScript"
+    }
+};
