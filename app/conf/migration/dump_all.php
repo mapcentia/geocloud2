a377f9dd499ae328f00a465223004685c0e31f42
Martin Høgh <mh@mapcentia.com>
2014-07-09 10:49:10 +0200

    dump_all script

--- app/conf/migration/dump_all.php
+++ app/conf/migration/dump_all.php
@@ -1,21 +1,18 @@
+#!/usr/bin/php
 <?php
-include("../conf/main.php");
-include("../libs/functions.php");
-include("../models/users.php");
-//echo "test";
-$user = new User(null);
-$all = $user->getAll();
-//print_r($all);
-$i = 0;
-foreach ($all['data'] as $value) {
-    $db= $value['screenname'];
-    if ($db != "template1" AND $db != "template0" AND $db != "postgres" AND $db != "postgis_template" AND $db != "mhoegh" AND $db != "mygeocloud" /*AND $i<2*/) {
-        $cmd = "pg_dump -h localhost -p 5432 -U postgres -Fc -b -f '/home/mh/backup/{$db}.backup' {$db}\n";
+header("Content-type: text/plain");
+include_once("../App.php");
+new \app\conf\App();
+$database = new \app\models\Database();
+$arr = $database->listAllDbs();
+$targetDir = \app\conf\App::$param["path"]."app/tmp/backup";
+if (!file_exists($mainDir)) {
+    @mkdir($targetDir);
+}
+foreach ($arr['data'] as $db) {
+    if ($db != "template1" AND $db != "template0" AND $db != "postgres" AND $db != "postgis_template" AND $db != "mapcentia") {
+        echo $db."\n";
+        $cmd = "pg_dump -h localhost -p 5432 -U postgres -Fc -b -f '{$targetDir}/{$db}.bak' {$db}\n";
         exec($cmd);
-        $cmd = "rsync -e 'ssh -i us1.pem' -avz /home/mh/backup/{$db}.backup ubuntu@us1.mapcentia.com:/home/mh/upload\n";
-        echo $cmd;
-        //exec($cmd);
-        //unlink("/home/mh/backup/{$db}.backup");
-        $i++;
     }
 }
