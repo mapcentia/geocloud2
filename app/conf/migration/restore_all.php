efe174d5433a69690f3d0c5a69501e84fda4b78a
Martin Høgh <mh@mapcentia.com>
2014-07-09 13:32:53 +0200

    dump_all script

--- app/conf/migration/restore_all.php
+++ app/conf/migration/restore_all.php
@@ -12,14 +12,17 @@ if ($handle = opendir($targetDir)) {
             $db = $split[0];
             $cmd = "createdb {$db} -U postgres  -T template0 -l en_US.UTF-8";
             echo $cmd."\n";
+            exec($cmd);
             $cmd = "psql -d {$db} -c \"CREATE EXTENSION postgis;\" -U postgres";
             echo $cmd."\n";
+            exec($cmd);
             $cmd = "psql -d {$db} -c \"CREATE EXTENSION pgcrypto;\" -U postgres";
             echo $cmd."\n";
+            exec($cmd);
             $cmd = "/usr/share/postgresql/9.3/contrib/postgis-2.1/postgis_restore.pl {$entry} | psql {$db} -U postgres";
             echo $cmd."\n";
+            exec($cmd);
             echo "**************\n";
-            //exec($cmd);
         }
     }
     closedir($handle);
