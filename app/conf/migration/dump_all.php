81eb3a8666d7d5287e47601298708741a4ac5dd4
Martin Høgh <mh@mapcentia.com>
2014-07-09 12:54:40 +0200

    dump_all script

--- app/conf/migration/dump_all.php
+++ app/conf/migration/dump_all.php
@@ -13,7 +13,7 @@ foreach ($arr['data'] as $db) {
     if ($db != "template1" AND $db != "template0" AND $db != "postgres" AND $db != "postgis_template" AND $db != "mapcentia") {
         $cmd = "pg_dump -h localhost -p 5432 -U postgres -Fc -b -f '{$targetDir}/{$db}.bak' {$db}";
         echo $cmd."\n";
-        //exec($cmd);
+        exec($cmd);
         //$cmd = "rsync -e 'ssh -i file.pem' -avz {$targetDir}/{$db}.bak ubuntu@us1.mapcentia.com:/home/mh/upload\n";
         //echo $cmd;
     }
