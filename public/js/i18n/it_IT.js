848792ac26fd5fc6873883e35a59e4511c95f47f
Andrea Borruso <aborruso@gmail.com>
2014-08-06 15:11:40 +0200

    Create it_IT.js
    I have made the italian translation

---
+++ public/js/i18n/it_IT.js
@@ -0,0 +1,31 @@
+var gc2i18n;
+gc2i18n = {
+    dict: {
+        /* Start of viewer and widget */
+        "Legend": "Legenda",
+        "Property": "Proprietà",
+        "Value": "Valore",
+        "Pop up": "Pop up",
+        "Layers": "Layer",
+        "Baselayers": "Baselayer",
+        "Share": "Condividi",
+        "Info": "Info",
+        "Close": "Chiudi",
+        "Back": "Indietro",
+        "Share the map": "Condividi la mappa",
+        "URL to this map": "L'URL a questa mappa",
+        "Search": "Cerca",
+        "Iframe with this map to embed on web page": "Un iframe con questa mappa per fare l'embed in una pagina web",
+        "URL to a static PNG image of this map": "L'URL a un'immagine PNG statica di questa mappa",
+        "JavaScript for an application": "Incorpora questa mappa come JavaScript",
+        /* Start of Admin */
+        "Map": "Mappa",
+        "Advanced": "Avanzate",
+        "Services": "Servizi",
+        "Clear tile cache": "Cancella la cache dei tile",
+        "New layer": "Nuovo layer",
+        "Move layers": "Muovi i layer",
+        "Delete layers": "Cancella i layer",
+        "Draw": "Disegna"
+    }
+};
