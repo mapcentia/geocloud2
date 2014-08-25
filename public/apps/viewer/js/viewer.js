5fb7b27dfbc3bd21e0c7a0a98da771a7240becfd
Martin Høgh <mh@mapcentia.com>
2014-08-25 13:44:53 +0200

    bug

--- public/apps/viewer/js/viewer.js
+++ public/apps/viewer/js/viewer.js
@@ -531,7 +531,7 @@ Viewer = function () {
                                                 if (property.querable) {
                                                     fieldLabel = (property.alias !== null && property.alias !== "") ? property.alias : name;
                                                     if (property.link) {
-                                                        out.push([name, property.sort_id, fieldLabel, "<a target='_blank' href='" + property.linkprefix + feature.properties[name] + "'>" + feature.properties[name] + "</a>"]);
+                                                        out.push([name, property.sort_id, fieldLabel, "<a target='_blank' href='" + ((property.linkprefix !== null) ? property.linkprefix : "") + feature.properties[name] + "'>" + feature.properties[name] + "</a>"]);
                                                     }
                                                     else {
                                                         out.push([name, property.sort_id, fieldLabel, feature.properties[name]]);
