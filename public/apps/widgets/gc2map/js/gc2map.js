727aea7a54d33382af6064670ce59157ab068b4c
Martin Høgh <mh@mapcentia.com>
2014-11-12 09:41:13 +0100

    Can set postgis port for mapserver

--- public/apps/widgets/gc2map/js/gc2map.js
+++ public/apps/widgets/gc2map/js/gc2map.js
@@ -46,7 +46,7 @@ if (typeof gc2map === "undefined") {
                         typeof $().emulateTransitionEnd !== 'undefined' &&
                         typeof Hogan !== "undefined" &&
                         typeof window.setBaseLayers !== "undefined"
-                        ) {
+                    ) {
                         // Load Dependants
                         $.getScript(host + "/api/v3/js/geocloud.js?a439ffa6e8bc2dd0#grunt-cache-bust");
                         $.getScript(host + "/apps/widgets/gc2map/js/main.js?ea4e1fd6a6fef346#grunt-cache-bust");
@@ -62,12 +62,36 @@ if (typeof gc2map === "undefined") {
                         setTimeout(pollForDependencies, 10);
                     }
                 }());
-                $('<link/>').attr({ rel: 'stylesheet', type: 'text/css', href: host + '/apps/widgets/gc2map/css/bootstrap.css?cea63a515abbff15#grunt-cache-bust' }).appendTo('head');
-                $('<link/>').attr({ rel: 'stylesheet', type: 'text/css', href: host + '/apps/widgets/gc2map/css/bootstrap-alert.css?a849390f2a859c66#grunt-cache-bust' }).appendTo('head');
-                $('<link/>').attr({ rel: 'stylesheet', type: 'text/css', href: host + '/apps/widgets/gc2map/css/non-responsive.css?3a2e6af4eca91b40#grunt-cache-bust' }).appendTo('head');
-                $('<link/>').attr({ rel: 'stylesheet', type: 'text/css', href: host + '/apps/widgets/gc2map/css/styles.css?fe15deedc44b317d#grunt-cache-bust' }).appendTo('head');
-                $('<link/>').attr({ rel: 'stylesheet', type: 'text/css', href: 'https://netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.min.css' }).appendTo('head');
-                $('<link/>').attr({ rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' }).appendTo('head');
+                $('<link/>').attr({
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: host + '/apps/widgets/gc2map/css/bootstrap.css?cea63a515abbff15#grunt-cache-bust'
+                }).appendTo('head');
+                $('<link/>').attr({
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: host + '/apps/widgets/gc2map/css/bootstrap-alert.css?a849390f2a859c66#grunt-cache-bust'
+                }).appendTo('head');
+                $('<link/>').attr({
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: host + '/apps/widgets/gc2map/css/non-responsive.css?3a2e6af4eca91b40#grunt-cache-bust'
+                }).appendTo('head');
+                $('<link/>').attr({
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: host + '/apps/widgets/gc2map/css/styles.css?fe15deedc44b317d#grunt-cache-bust'
+                }).appendTo('head');
+                $('<link/>').attr({
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: 'https://netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.min.css'
+                }).appendTo('head');
+                $('<link/>').attr({
+                    rel: 'stylesheet',
+                    type: 'text/css',
+                    href: 'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300'
+                }).appendTo('head');
             } else {
                 setTimeout(pollForjQuery, 10);
             }
@@ -85,11 +109,12 @@ if (typeof gc2map === "undefined") {
                     key: null,
                     clickDistance: 5,
                     baseLayers: null,
-                    callBack: function () {}
+                    callBack: function () {
+                    }
                 },
                 prop,
-                divs = document.getElementsByTagName('div'),
-                div = divs[divs.length - 1],
+                divs,
+                div,
                 gc2RandId;
             if (conf) {
                 for (prop in conf) {
@@ -100,7 +125,13 @@ if (typeof gc2map === "undefined") {
                 var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             });
-            div.setAttribute("id", gc2RandId);
+            if (defaults.key === null) {
+                divs = document.getElementsByTagName('div');
+                div = divs[divs.length - 1];
+                div.setAttribute("id", gc2RandId);
+            } else {
+                div = document.getElementById(defaults.key);
+            }
             div.classList.add("gc2map");
             div.style.width = defaults.width;
             div.style.height = defaults.height;
@@ -159,7 +190,110 @@ if (typeof gc2map === "undefined") {
     }());
 }
 /*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
-;if("document" in self&&!("classList" in document.createElement("_"))){(function(j){"use strict";if(!("Element" in j)){return}var a="classList",f="prototype",m=j.Element[f],b=Object,k=String[f].trim||function(){return this.replace(/^\s+|\s+$/g,"")},c=Array[f].indexOf||function(q){var p=0,o=this.length;for(;p<o;p++){if(p in this&&this[p]===q){return p}}return -1},n=function(o,p){this.name=o;this.code=DOMException[o];this.message=p},g=function(p,o){if(o===""){throw new n("SYNTAX_ERR","An invalid or illegal string was specified")}if(/\s/.test(o)){throw new n("INVALID_CHARACTER_ERR","String contains an invalid character")}return c.call(p,o)},d=function(s){var r=k.call(s.getAttribute("class")||""),q=r?r.split(/\s+/):[],p=0,o=q.length;for(;p<o;p++){this.push(q[p])}this._updateClassName=function(){s.setAttribute("class",this.toString())}},e=d[f]=[],i=function(){return new d(this)};n[f]=Error[f];e.item=function(o){return this[o]||null};e.contains=function(o){o+="";return g(this,o)!==-1};e.add=function(){var s=arguments,r=0,p=s.length,q,o=false;do{q=s[r]+"";if(g(this,q)===-1){this.push(q);o=true}}while(++r<p);if(o){this._updateClassName()}};e.remove=function(){var t=arguments,s=0,p=t.length,r,o=false;do{r=t[s]+"";var q=g(this,r);if(q!==-1){this.splice(q,1);o=true}}while(++s<p);if(o){this._updateClassName()}};e.toggle=function(p,q){p+="";var o=this.contains(p),r=o?q!==true&&"remove":q!==false&&"add";if(r){this[r](p)}return !o};e.toString=function(){return this.join(" ")};if(b.defineProperty){var l={get:i,enumerable:true,configurable:true};try{b.defineProperty(m,a,l)}catch(h){if(h.number===-2146823252){l.enumerable=false;b.defineProperty(m,a,l)}}}else{if(b[f].__defineGetter__){m.__defineGetter__(a,i)}}}(self))};
+;
+if ("document" in self && !("classList" in document.createElement("_"))) {
+    (function (j) {
+        "use strict";
+        if (!("Element" in j)) {
+            return
+        }
+        var a = "classList", f = "prototype", m = j.Element[f], b = Object, k = String[f].trim || function () {
+                return this.replace(/^\s+|\s+$/g, "")
+            }, c = Array[f].indexOf || function (q) {
+                var p = 0, o = this.length;
+                for (; p < o; p++) {
+                    if (p in this && this[p] === q) {
+                        return p
+                    }
+                }
+                return -1
+            }, n = function (o, p) {
+            this.name = o;
+            this.code = DOMException[o];
+            this.message = p
+        }, g = function (p, o) {
+            if (o === "") {
+                throw new n("SYNTAX_ERR", "An invalid or illegal string was specified")
+            }
+            if (/\s/.test(o)) {
+                throw new n("INVALID_CHARACTER_ERR", "String contains an invalid character")
+            }
+            return c.call(p, o)
+        }, d = function (s) {
+            var r = k.call(s.getAttribute("class") || ""), q = r ? r.split(/\s+/) : [], p = 0, o = q.length;
+            for (; p < o; p++) {
+                this.push(q[p])
+            }
+            this._updateClassName = function () {
+                s.setAttribute("class", this.toString())
+            }
+        }, e = d[f] = [], i = function () {
+            return new d(this)
+        };
+        n[f] = Error[f];
+        e.item = function (o) {
+            return this[o] || null
+        };
+        e.contains = function (o) {
+            o += "";
+            return g(this, o) !== -1
+        };
+        e.add = function () {
+            var s = arguments, r = 0, p = s.length, q, o = false;
+            do {
+                q = s[r] + "";
+                if (g(this, q) === -1) {
+                    this.push(q);
+                    o = true
+                }
+            } while (++r < p);
+            if (o) {
+                this._updateClassName()
+            }
+        };
+        e.remove = function () {
+            var t = arguments, s = 0, p = t.length, r, o = false;
+            do {
+                r = t[s] + "";
+                var q = g(this, r);
+                if (q !== -1) {
+                    this.splice(q, 1);
+                    o = true
+                }
+            } while (++s < p);
+            if (o) {
+                this._updateClassName()
+            }
+        };
+        e.toggle = function (p, q) {
+            p += "";
+            var o = this.contains(p), r = o ? q !== true && "remove" : q !== false && "add";
+            if (r) {
+                this[r](p)
+            }
+            return !o
+        };
+        e.toString = function () {
+            return this.join(" ")
+        };
+        if (b.defineProperty) {
+            var l = {get: i, enumerable: true, configurable: true};
+            try {
+                b.defineProperty(m, a, l)
+            } catch (h) {
+                if (h.number === -2146823252) {
+                    l.enumerable = false;
+                    b.defineProperty(m, a, l)
+                }
+            }
+        } else {
+            if (b[f].__defineGetter__) {
+                m.__defineGetter__(a, i)
+            }
+        }
+    }(self))
+}
+;
 
 
 
