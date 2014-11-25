59bf0d6069d6136bfd3a3a08af582f37a0b465c4
Martin Høgh <mh@mapcentia.com>
2014-11-25 21:47:29 +0100

    German translation

--- app/api/v1/Baselayerjs.php
+++ app/api/v1/Baselayerjs.php
@@ -31,7 +31,7 @@ class Baselayerjs extends \app\inc\Controller
             echo "window.mapAttribution = '" . \app\conf\App::$param['mapAttribution'] . "';\n";
         }
 
-        $locales = array("en_US", "da_DK", "fr_FR", "es_ES", "it_IT");
+        $locales = array("en_US", "da_DK", "fr_FR", "es_ES", "it_IT", "de_DE");
         $arr = explode(",", $_SERVER['HTTP_ACCEPT_LANGUAGE']);
         //echo $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "\n";
         //echo $requestedLan."\n";
