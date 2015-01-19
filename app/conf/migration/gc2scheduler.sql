48885a51947d6f9566de90c6b08752a7c23f9020
Martin Høgh <mh@mapcentia.com>
2015-01-19 15:21:24 +0100

    no message

---
+++ app/conf/migration/gc2scheduler.sql
@@ -0,0 +1,135 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: gc2scheduler; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE gc2scheduler WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+
+ALTER DATABASE gc2scheduler OWNER TO postgres;
+
+\connect gc2scheduler
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: jobs; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE jobs (
+    id integer NOT NULL,
+    name character varying(255),
+    url character varying(255),
+    cron character varying(255),
+    schema character varying(255),
+    epsg character varying(255),
+    type character varying(255),
+    min character varying,
+    hour character varying,
+    dayofmonth character varying,
+    month character varying,
+    dayofweek character varying,
+    encoding character varying(255),
+    lastcheck boolean,
+    lasttimestamp timestamp with time zone,
+    db character varying(255),
+    extra character varying(255)
+);
+
+
+ALTER TABLE public.jobs OWNER TO postgres;
+
+--
+-- Name: jobs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE jobs_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.jobs_id_seq OWNER TO postgres;
+
+--
+-- Name: jobs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE jobs_id_seq OWNED BY jobs.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY jobs ALTER COLUMN id SET DEFAULT nextval('jobs_id_seq'::regclass);
+
+
+--
+-- Data for Name: jobs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY jobs (id, name, url, cron, schema, epsg, type, min, hour, dayofmonth, month, dayofweek, encoding, lastcheck, lasttimestamp, db, extra) FROM stdin;
+11	ewrer	sdsd	\N	pusd	sd	POINT	*	*	*	*	*	UTF8	f	\N	dsdsd	
+12	kulturarv	http://www.kulturarv.dk/ffpublic/wfs?version=1.0.0&SERVICE=WFS&REQUEST=GETFEATURE&TYPENAME=fourf:fundogfortidsminder_areal_ka&BBOX=704275.26,6177444.42,716199.48,6185056.18	\N	public	25832	MULTIPOLYGON	*/1	*	*	*	*	UTF8	t	2015-01-08 21:15:14+00	mydb	{"name":"hej","value":"test"}
+\.
+
+
+--
+-- Name: jobs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('jobs_id_seq', 15, true);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
