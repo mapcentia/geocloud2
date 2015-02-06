1fafd3403e71b889f1b9b29ed35131c8180b0b6f
Martin Høgh <mh@mapcentia.com>
2015-02-06 21:58:28 +0100

    no message

--- app/scripts/pg2es.js
+++ app/scripts/pg2es.js
@@ -7,30 +7,36 @@ winston.add(winston.transports.File, { filename: '/var/www/geocloud2/public/logs
 nconf.argv();
 var db = (nconf.get()._[0]);
 var host = nconf.get("host") || "127.0.0.1";
+var esHost = nconf.get("es-host") || "127.0.0.1";
 var user = nconf.get("user") || "postgres";
 var key = nconf.get("key") || null;
 var pgConString = "postgres://" + user + "@" + host + "/" + db;
 
-if (!db) {
-    console.log("usage: test.js [database]");
+if (nconf.get("help") || !db) {
+    console.log("usage:");
+    console.log("  pg2es.js database [options]");
+    console.log("Options:");
+    console.log("  --host       PostGreSQL host. Default 127.0.0.1");
+    console.log("  --user       PostGreSQL user. Default postgres");
+    console.log("  --es-host    Elasticsearch host. Default 127.0.0.1");
     process.exit(1);
-} else {
-    console.log("Listen on database: " + db + " @ " + host + " with user " + user);
 }
+
+console.log("Listen on database: " + db + " @ " + host + " with user " + user);
 
 pg.connect(pgConString, function (err, client) {
     if (err) {
         console.log(err);
     }
     client.on('notification', function (msg) {
-        var uri, split = msg.payload.split(","), url, host = "127.0.0.1";
+        var uri, split = msg.payload.split(","), url;
         if (split[0] === "UPDATE" || split[0] === "INSERT") {
             uri = "upsert/" + db + "/" + split[1] + "/" + split[2] + "/" + split[3] + "/" + split[4];
         }
         if (split[0] === "DELETE") {
             uri = "delete/" + db + "/" + split[1] + "/" + split[2] + "/" + split[3];
         }
-        url = "http://" + host + "/api/v1/elasticsearch/" + uri;
+        url = "http://" + esHost + "/api/v1/elasticsearch/" + uri;
         if (key) {
             url = url + "?key=" + key;
         }
