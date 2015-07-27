63096e7c0f751045500386e73faebc51e9ad46c2
Martin Høgh <mh@mapcentia.com>
2015-07-27 21:27:43 +0200

    Restricted extent comeback

--- app/models/Layer.php
+++ app/models/Layer.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\models;
 
+use \app\conf\App;
+
 class Layer extends \app\models\Table
 {
     function __construct()
@@ -138,7 +140,7 @@ class Layer extends \app\models\Table
                 if (mb_substr($type, 0, 1, 'utf-8') == "_") {
                     $type = "a" . $type;
                 }
-                $url = "http://127.0.0.1:9200/{$this->postgisdb}_{$row['f_table_schema']}/{$type}/";
+                $url = (App::$param['esHost'] ?: "http://127.0.0.1") . ":9200/{$this->postgisdb}_{$row['f_table_schema']}/{$type}/";
                 $ch = curl_init($url);
                 curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
                 curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
