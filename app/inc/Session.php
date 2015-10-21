cfc47e689422c7ae25318b3402ba9afd9b89c106
Martin Høgh <mh@mapcentia.com>
2015-10-21 08:52:37 +0200

    Session API

--- app/inc/Session.php
+++ app/inc/Session.php
@@ -69,4 +69,5 @@ class Session
             }
         }
     }
+
 }
---
+++ app/models/Session.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use app\inc\Model;
+
+class Session extends Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function start($sUserID, $pw)
+    {
+        $sPassword = Setting::encryptPw($pw);
+
+        $sQuery = "SELECT * FROM users WHERE (screenname = :sUserID OR email = :sUserID) AND pw = :sPassword";
+        $res = $this->prepare($sQuery);
+        $res->execute(array(":sUserID" => $sUserID, ":sPassword" => $sPassword));
+        $row = $this->fetchRow($res);
+
+        if ($row['screenname']) {
+            // Login successful.
+            $_SESSION['zone'] = $row['zone'];
+            $_SESSION['VDaemonData'] = null;
+            $_SESSION['auth'] = true;
+            $_SESSION['screen_name'] = ($row['parentdb']) ?: $sUserID;
+            $_SESSION['subuser'] = ($row['parentdb']) ? $row['screenname'] : false;
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['usergroup'] = $row['usergroup'] ?: false;
+            $_SESSION['created'] = strtotime($row['created']);
+
+            $response['success'] = true;
+            $response['message'] = "Session started";
+            $response['user'] = $row['screenname'];
+
+        } else {
+            $response['success'] = false;
+            $response['message'] = "Session not started";
+            $response['code'] = "401";
+        }
+        return $response;
+    }
+    public function stop(){
+        session_unset();
+        $response['success'] = true;
+        $response['message'] = "Session stopped";
+        return $response;
+
+    }
+}
