a49c68c1562e7fcea1b46c21de324dc0b3205813
Martin Høgh <mh@mapcentia.com>
2016-07-09 13:27:53 +0200

    no message

--- public/apps/mapclient/js/config.js
+++ public/apps/mapclient/js/config.js
@@ -849,6 +849,174 @@ MapCentia.init = function () {
         {
             type: "any",
             options: {
+                text: "",
+                iconCls: "icon-draw-text",
+                tooltip: "Søg på adresse eller matrikelnr.",
+                id: "customSearch",
+                toggleGroup: "conflict",
+                handler: function (objRef) {
+                    cleanUpConflict();
+                    customSearchWin = new Ext.Window({
+                        title: "Find",
+                        layout: 'fit',
+                        width: 300,
+                        height: 500,
+                        plain: true,
+                        closeAction: 'close',
+                        border: false,
+                        html: '<div style="padding: 5px" id="searchContent"><input class="typeahead" type="text" id="custom-search" name="custom-search" value="" placeholder="Søg på adresse eller matrikelnr." /></div>',
+                        x: 300,
+                        y: 40,
+                        listeners: {
+                            hide: {
+                                fn: function (el, e) {
+                                    cleanUpConflict();
+                                    Ext.getCmp("customSearch").toggle(false);
+                                }
+                            }
+                        }
+                    });
+                    if (typeof(objRef) === "object") {
+                        customSearchWin.show(objRef);
+                    } else {
+                        customSearchWin.show();
+                    }
+                    (function (me) {
+                        var type1, type2, gids = [], searchString,
+                            komKode = "727",
+                            placeStore = new geocloud.geoJsonStore({
+                                host: "http://eu1.mapcentia.com",
+                                db: "dk",
+                                sql: null,
+                                pointToLayer: null,
+                                onLoad: function () {
+                                    setTimeout(function () {
+                                        MapCentia.gc2.zoomToExtentOfgeoJsonStore(placeStore);
+                                    }, 300);
+                                    deactivateControllers();
+                                    var wkt = new OpenLayers.Format.WKT().write(placeStore.layer.features[0]);
+                                    conflict(wkt);
+                                }
+                            });
+                        $('#custom-search').typeahead({
+                            highlight: false
+                        }, {
+                            name: 'adresse',
+                            displayKey: 'value',
+                            templates: {
+                                header: '<h2 class="typeahead-heading">Adresser</h2>'
+                            },
+                            source: function (query, cb) {
+                                if (query.match(/\d+/g) === null && query.match(/\s+/g) === null) {
+                                    type1 = "vejnavn,bynavn";
+                                }
+                                if (query.match(/\d+/g) === null && query.match(/\s+/g) !== null) {
+                                    type1 = "vejnavn_bynavn";
+                                }
+                                if (query.match(/\d+/g) !== null) {
+                                    type1 = "adresse";
+                                }
+                                var names = [];
+
+                                (function ca() {
+                                    $.ajax({
+                                        url: 'http://eu1.mapcentia.com/api/v1/elasticsearch/search/dk/aws4/' + type1,
+                                        data: '&q={"query":{"filtered":{"query":{"query_string":{"default_field":"string","query":"' + encodeURIComponent(query.toLowerCase().replace(",", "")) + '","default_operator":"AND"}},"filter":{"term":{"kommunekode":"0' + komKode + '"}}}}}',
+                                        contentType: "application/json; charset=utf-8",
+                                        scriptCharset: "utf-8",
+                                        dataType: 'jsonp',
+                                        jsonp: 'jsonp_callback',
+                                        success: function (response) {
+                                            $.each(response.hits.hits, function (i, hit) {
+                                                var str = hit._source.properties.string;
+                                                gids[str] = hit._source.properties.gid;
+                                                names.push({value: str});
+                                            });
+                                            if (names.length === 1 && (type1 === "vejnavn,bynavn" || type1 === "vejnavn_bynavn")) {
+                                                type1 = "adresse";
+                                                names = [];
+                                                gids = [];
+                                                ca();
+                                            } else {
+                                                cb(names);
+                                            }
+                                        }
+                                    });
+                                })();
+                            }
+                        }, {
+                            name: 'matrikel',
+                            displayKey: 'value',
+                            templates: {
+                                header: '<h2 class="typeahead-heading">Matrikel</h2>'
+                            },
+                            source: function (query, cb) {
+                                var names = [];
+                                type2 = (query.match(/\d+/g) != null) ? "jordstykke" : "ejerlav";
+                                (function ca() {
+                                    $.ajax({
+                                        url: 'http://eu1.mapcentia.com/api/v1/elasticsearch/search/dk/matrikel/' + type2,
+                                        data: '&q={"query":{"filtered":{"query":{"query_string":{"default_field":"string","query":"' + encodeURIComponent(query.toLowerCase()) + '","default_operator":"AND"}},"filter":{"term":{"komkode":"' + komKode + '"}}}}}',
+                                        contentType: "application/json; charset=utf-8",
+                                        scriptCharset: "utf-8",
+                                        dataType: 'jsonp',
+                                        jsonp: 'jsonp_callback',
+                                        success: function (response) {
+                                            $.each(response.hits.hits, function (i, hit) {
+                                                var str = hit._source.properties.string;
+                                                gids[str] = hit._source.properties.gid;
+                                                names.push({value: str});
+                                            });
+                                            if (names.length === 1 && (type2 === "ejerlav")) {
+                                                type2 = "jordstykke";
+                                                names = [];
+                                                gids = [];
+                                                ca();
+                                            } else {
+                                                cb(names);
+                                            }
+                                        }
+                                    })
+                                })();
+                            }
+                        });
+                        $('#custom-search').bind('typeahead:selected', function (obj, datum, name) {
+                            if ((type1 === "adresse" && name === "adresse") || (type2 === "jordstykke" && name === "matrikel")) {
+                                cleanUpConflict();
+                                try {
+                                    gridWin.close();
+                                }
+                                catch (e) {
+                                }
+                                try {
+                                    gridPanel.remove(grid);
+                                }
+                                catch (e) {
+                                }
+                                placeStore.reset();
+
+                                if (name === "matrikel") {
+                                    placeStore.sql = "SELECT gid,the_geom,ST_asgeojson(ST_transform(the_geom,4326)) as geojson FROM matrikel.jordstykke WHERE gid=" + gids[datum.value];
+                                }
+                                if (name === "adresse") {
+                                    placeStore.sql = "SELECT gid,the_geom,ST_asgeojson(ST_transform(the_geom,4326)) as geojson FROM adresse.adgang4 WHERE gid=" + gids[datum.value];
+                                }
+                                searchString = datum.value;
+                                placeStore.load();
+                            } else {
+                                setTimeout(function () {
+                                    $(".typeahead").val(datum.value + " ").trigger("paste").trigger("input");
+                                }, 100);
+                            }
+                        });
+                    }());
+
+                }
+            }
+        },
+        {
+            type: "any",
+            options: {
                 text: '',
                 tooltip: 'Search with Google Places',
                 iconCls: 'icon-map-magnify',
