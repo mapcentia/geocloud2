b4a9371e4f23ba0c2a90ad4f1879785b580c54bb
Martin Høgh <mh@mapcentia.com>
2016-10-26 09:12:42 +0200

    pg2es.js is more stable and will survive PG restart

--- app/scripts/pg2es.js
+++ app/scripts/pg2es.js
@@ -2,7 +2,8 @@ var pg = require('pg');
 var nconf = require('nconf');
 var request = require("request");
 var winston = require('winston');
-winston.add(winston.transports.File, { filename: '/var/www/geocloud2/public/logs/pg2es.log' });
+winston.add(winston.transports.File, {filename: '/var/www/geocloud2/public/logs/pg2es.log'});
+
 
 nconf.argv();
 var db = (nconf.get()._[0]);
@@ -10,7 +11,6 @@ var host = nconf.get("host") || "127.0.0.1";
 var esHost = nconf.get("es-host") || "127.0.0.1";
 var user = nconf.get("user") || "postgres";
 var key = nconf.get("key") || null;
-var pgConString = "postgres://" + user + "@" + host + "/" + db;
 
 if (nconf.get("help") || !db) {
     console.log("usage:");
@@ -19,15 +19,26 @@ if (nconf.get("help") || !db) {
     console.log("  --host       PostGreSQL host. Default 127.0.0.1");
     console.log("  --user       PostGreSQL user. Default postgres");
     console.log("  --es-host    Elasticsearch host. Default 127.0.0.1");
+    console.log("  --key        GC2 API key");
     process.exit(1);
 }
 
-console.log("Listen on database: " + db + " @ " + host + " with user " + user);
-
-pg.connect(pgConString, function (err, client) {
-    if (err) {
-        console.log(err);
-    }
+/**
+ * @type {{user: (any), database: *, host: (any), port: number, max: number, idleTimeoutMillis: number}}
+ */
+var config = {
+    user: user,
+    database: db,
+    host: host,
+    port: 5432,
+    max: 1, // ONLY ONE CLIENT IN THE POOL
+    idleTimeoutMillis: 3000 // how long a client is allowed to remain idle before being closed
+};
+/**
+ * @param client
+ */
+var start = function (client) {
+    console.log("Listen on database: " + db + "@" + host + " with user " + user);
     client.on('notification', function (msg) {
         var uri, split = msg.payload.split(","), url;
         if (split[0] === "UPDATE" || split[0] === "INSERT") {
@@ -43,7 +54,6 @@ pg.connect(pgConString, function (err, client) {
         request.get(url, function (err, res, body) {
             if (!err) {
                 var resultsObj = JSON.parse(body);
-                //console.log(resultsObj);
                 winston.log('info', resultsObj.message, resultsObj);
             } else {
                 winston.log('error', err);
@@ -52,5 +62,26 @@ pg.connect(pgConString, function (err, client) {
         });
 
     });
-    var query = client.query("LISTEN _gc2_notify_transaction");
+    client.query("LISTEN _gc2_notify_transaction");
+};
+/**
+ * @type {pg.Pool}
+ */
+var pool = new pg.Pool(config);
+
+pool.connect(function (err, client, done) {
+    if (err) {
+        console.log(err);
+    } else {
+        start(client);
+    }
+});
+
+pool.on('error', function (err, client) {
+    console.error(err.message);
+    pool.connect(function (err, client, done) {
+        if (!err) {
+            start(client);
+        }
+    });
 });
