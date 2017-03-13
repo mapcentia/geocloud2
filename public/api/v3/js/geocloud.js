bdc40fe200edc86d8fe05ef3955c0e3fec850426
Martin Høgh <mh@mapcentia.com>
2017-03-13 15:12:14 +0100

    no message

--- public/api/v3/js/geocloud.js
+++ public/api/v3/js/geocloud.js
@@ -546,6 +546,9 @@ geocloud = (function () {
                     tileSize: defaults.tileSize
                 });
                 l.id = layer;
+		if (defaults.loadEvent) {
+                    l.on("load", defaults.loadEvent);
+                }
                 break;
         }
         return l;
@@ -2786,63 +2789,97 @@ if (geocloud.MAPLIB === "leaflet") {
 }
 
 if (geocloud.MAPLIB === "leaflet") {
+    (function () {
+        var bounce = false, isSet=true, first = true;
+        /* Patch for Leaflet 0.7.3  */
+        if (L.TileLayer && L.TileLayer.WMS) {
+            /* Set tileSize option to 9999 (or greater) to switch it to single tile mode */
+            L.TileLayer.WMS.prototype.getTileUrl2 = function (tilePoint) {
+                var me = this;
 
-    /* Patch for Leaflet 0.7.3  */
-    if (L.TileLayer && L.TileLayer.WMS) {
-        /* Set tileSize option to 9999 (or greater) to switch it to single tile mode */
-        L.TileLayer.WMS.prototype.getTileUrl2 = function (tilePoint) {
-            if (!this._singleTile) {
-                return this.getTileUrl(tilePoint);
-            }
-            var tmp = this._map.getSize();
-            this.wmsParams.width = tmp.x;
-            this.wmsParams.height = tmp.y;
-            tmp = this._map.getBounds();
-
-            var nw = this._crs.project(tmp.getNorthWest());
-            var se = this._crs.project(tmp.getSouthEast());
-            var bbox = this._wmsVersion >= 1.3 && this._crs === L.CRS.EPSG4326 ?
-                [se.y, nw.x, nw.y, se.x].join(',') :
-                [nw.x, se.y, se.x, nw.y].join(',');
-            url = L.Util.template(this._url, {s: this._getSubdomain(tilePoint)});
-            return url + L.Util.getParamString(this.wmsParams, url, true) + '&BBOX=' + bbox;
-        }
-        L.TileLayer.WMS.prototype._loadTile = function (tile, tilePoint) {
-            if (this._singleTile) {
-                var siz = this._map.getSize();
-                tile.style.width = siz.x + 'px';
-                tile.style.height = siz.y + 'px';
-            }
-            tile._layer = this;
-            tile.onload = function () {
-                if (this._layer._singleTile) {
-                    this._layer._reset({hard: true});
-                    this._layer._tileContainer.appendChild(this);
+                if (!this._singleTile) {
+                    return this.getTileUrl(tilePoint);
                 }
-                L.TileLayer.WMS.prototype._tileOnLoad.call(this);
-            };
-            tile.onerror = this._tileOnError;
-            this._adjustTilePoint(tilePoint);
-            tile.src = this.getTileUrl2(tilePoint);
-            this.fire('tileloadstart', {tile: tile, url: tile.src});
-        }
-        L.TileLayer.WMS.prototype._addTilesFromCenterOut = function (bounds) {
-            this._singleTile = this.options.tileSize >= 9999;
-            if (!this._singleTile) {
-                return L.TileLayer.prototype._addTilesFromCenterOut.call(this, bounds);
-            }
+                var tmp = this._map.getSize();
+                this.wmsParams.width = tmp.x;
+                this.wmsParams.height = tmp.y;
+                tmp = this._map.getBounds();
 
-            var fragment = document.createDocumentFragment();
-            this.fire('loading');
-            this._addTile(new L.Point(bounds.min.x, bounds.min.y), fragment);
-            this._tileContainer.appendChild(fragment);
-        }
-        L.TileLayer.WMS.prototype._getTilePos = function (tilePoint) {
-            if (!this._singleTile) {
-                return L.TileLayer.prototype._getTilePos.call(this, tilePoint);
+                var nw = this._crs.project(tmp.getNorthWest());
+                var se = this._crs.project(tmp.getSouthEast());
+                var bbox = this._wmsVersion >= 1.3 && this._crs === L.CRS.EPSG4326 ?
+                    [se.y, nw.x, nw.y, se.x].join(',') :
+                    [nw.x, se.y, se.x, nw.y].join(',');
+                url = L.Util.template(this._url, {s: this._getSubdomain(tilePoint)});
+
+                if (!bounce && !first) {
+                    bounce = true;
+                    setTimeout(function () {
+                        first = true;
+
+                        bounce = false
+                    }, 1000)
+                    isSet = true;
+                    return url + L.Util.getParamString(this.wmsParams, url, true) + '&BBOX=' + bbox;
+                } else {
+                    isSet = false;
+                    setTimeout(function () {
+                        first = false;
+
+                        if (!isSet) {
+                            me.redraw();
+                        }
+                    }, 1000)
+                    return false
+                }
             }
-            return this._map.getPixelBounds().min.subtract(this._map.getPixelOrigin())
+            L.TileLayer.WMS.prototype._loadTile = function (tile, tilePoint) {
+                var me = this;
+                if (this._singleTile) {
+                    var siz = this._map.getSize();
+                    tile.style.width = siz.x + 'px';
+                    tile.style.height = siz.y + 'px';
+                }
+                tile._layer = this;
+
+
+                tile.onload = function () {
+                    if (this._layer._singleTile) {
+                        this._layer._reset({hard: true});
+                        this._layer._tileContainer.appendChild(this);
+			this._layer.fire("load");
+                    }
+                    L.TileLayer.WMS.prototype._tileOnLoad.call(this);
+                };
+                tile.onerror = this._tileOnError;
+                this._adjustTilePoint(tilePoint);
+                var url = this.getTileUrl2(tilePoint);
+                if (url) {
+                    console.info(url);
+                    tile.src = url
+                } else {
+                    console.info("Skipping single tile");
+                }
+                this.fire('tileloadstart', {tile: tile, url: tile.src});
+            }
+            L.TileLayer.WMS.prototype._addTilesFromCenterOut = function (bounds) {
+                this._singleTile = this.options.tileSize >= 9999;
+                if (!this._singleTile) {
+                    return L.TileLayer.prototype._addTilesFromCenterOut.call(this, bounds);
+                }
+
+                var fragment = document.createDocumentFragment();
+                this.fire('loading');
+                this._addTile(new L.Point(bounds.min.x, bounds.min.y), fragment);
+                this._tileContainer.appendChild(fragment);
+            }
+            L.TileLayer.WMS.prototype._getTilePos = function (tilePoint) {
+                if (!this._singleTile) {
+                    return L.TileLayer.prototype._getTilePos.call(this, tilePoint);
+                }
+                return this._map.getPixelBounds().min.subtract(this._map.getPixelOrigin())
+            }
         }
-    }
+    }());
 }
 
