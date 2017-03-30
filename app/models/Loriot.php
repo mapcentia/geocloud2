4d74b945aa5cc893372035e97d8a4e2eb6e1172c
Martin Høgh <mh@mapcentia.com>
2017-03-30 22:34:11 +0200

    no message

--- app/models/Loriot.php
+++ app/models/Loriot.php
@@ -12,20 +12,40 @@ class Loriot extends Model
 
     public function insert($data, $table)
     {
+        $data = array_change_key_case($data, CASE_LOWER);
+
+        $defaults = [
+            "cmd" => null,
+            "eui" => null,
+            "ts" => null,
+            "ack" => false,
+            "fcnt" => null,
+            "port" => null,
+            "encdata" => null,
+            "data" => null,
+        ];
+
+        foreach ($defaults as $key => $value) {
+            $defaults[$key] = $data[$key];
+        }
+
+        $defaults["ack"] = $defaults["ack"] ? "t" : "f";
+
         $response = array();
-        $sql = "INSERT INTO {$table}(cmd, eui, ts, ack, fcnt, port, encdata, data) VALUES (:cmd, :eui, :ts, :ack, :fcnt, :port, :encdata, :data) RETURNING *";
+        $sql = "INSERT INTO {$table}(cmd, eui, ts, ack, fcnt, port, encdata, data) VALUES (:cmd, :eui, to_timestamp(:ts), :ack, :fcnt, :port, :encdata, :data) RETURNING *";
         $res = $this->prepare($sql);
-        $arr = json_decode($data, true);
+
         try {
-            $res->execute($arr);
+            $res->execute($defaults);
         } catch (\PDOException $e) {
             $response["success"] = false;
             $response["message"] = $e->getMessage();
             $response["code"] = "401";
-            return $response;
 
         }
+
         $response["success"] = true;
-        $response["data"] = json_encode($arr);
+        $response["data"] = json_encode($defaults);
+        return $response;
     }
 }
