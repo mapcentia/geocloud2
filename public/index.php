5f3f16f69da5732586b4f5f698f515e7e913af8c
Martin Høgh <mh@mapcentia.com>
2017-03-30 21:32:05 +0200

    no message

--- public/index.php
+++ public/index.php
@@ -69,6 +69,7 @@ if (Input::getPath()->part(1) == "api") {
     Route::add("api/v1/collector");
     Route::add("api/v1/decodeimg");
     Route::add("api/v1/senti");
+    Route::add("api/v1/loriot");
     Route::add("api/v1/session", function () {
         Session::start();
         Database::setDb("mapcentia");
