a9264fdc7292f69a227ced630143d4fe8bed6c79
Martin Høgh <mh@mapcentia.com>
2018-08-21 12:53:16 +0200

    Set some vars

--- public/scheduler/report.php
+++ public/scheduler/report.php
@@ -115,6 +115,11 @@
 <body>
 <?php
 
+ini_set("display_errors", "Off");
+ini_set('memory_limit', '512M');
+ini_set('max_execution_time', 0);
+error_reporting(3);
+
 include_once(__DIR__ . "/../../app/conf/App.php");
 
 new \app\conf\App();
