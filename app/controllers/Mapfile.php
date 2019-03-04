e7e05f9e888fc9b41435a4d5c0b1417cd72586e2
Martin Høgh <mh@mapcentia.com>
2019-03-04 10:22:42 +0100

    Can now set, which SRS should be advertised in MapServer OWS

--- app/controllers/Mapfile.php
+++ app/controllers/Mapfile.php
@@ -78,17 +78,13 @@ class Mapfile extends \app\inc\Controller
             IMAGEPATH "<?php echo App::$param['path']; ?>/tmp"
             IMAGEURL "<?php echo App::$param['host']; ?>/tmp"
             METADATA
-                "ows_title"    "<?php echo $user; ?>'s OWS"
-                "ows_srs"    "EPSG:4326 EPSG:3857 EPSG:900913 EPSG:3044 EPSG:25832"
-                "ows_name"    "<?php echo $user; ?>"
+                "wms_title"    "<?php echo $user; ?>'s OWS"
+                "wms_srs"    <?php echo "\"" . (isset(App::$param['advertisedSrs']) ?  implode(" ", App::$param['advertisedSrs']) : "EPSG:4326 EPSG:3857 EPSG:900913 EPSG:3044 EPSG:25832") . "\"\n" ?>
+                "wms_name"    "<?php echo $user; ?>"
                 "wms_format"    "image/png"
                 "wms_onlineresource"    "http://<?php echo $_SERVER['HTTP_HOST']; ?>/ows/<?php echo Connection::$param['postgisdb']; ?>/<?php echo Connection::$param['postgisschema']; ?>/"
-                "wfs_onlineresource"    "http://<?php echo $_SERVER['HTTP_HOST']; ?>/ows/<?php echo Connection::$param['postgisdb']; ?>/<?php echo Connection::$param['postgisschema']; ?>/"
-                "ows_enable_request" "*"
                 "wms_enable_request" "*"
                 "ows_encoding" "UTF-8"
-                "wfs_namespace_prefix" "<?php echo $user; ?>"
-                "wfs_namespace_uri" "<?php echo App::$param['host']; ?>"
             END
         END
 
@@ -906,7 +902,7 @@ class Mapfile extends \app\inc\Controller
         WEB
             METADATA
                 "wfs_title"    "<?php echo $user; ?>'s OWS"
-                "wfs_srs"    "EPSG:4326 EPSG:3857 EPSG:900913 EPSG:3044 EPSG:25832"
+                "wfs_srs"    <?php echo "\"" . (isset(App::$param['advertisedSrs']) ?  implode(" ", App::$param['advertisedSrs']) : "EPSG:4326 EPSG:3857 EPSG:900913 EPSG:3044 EPSG:25832") . "\"\n" ?>
                 "wfs_name"    "<?php echo $user; ?>"
                 "wfs_onlineresource"    "http://<?php echo $_SERVER['HTTP_HOST']; ?>/ows/<?php echo Connection::$param['postgisdb']; ?>/<?php echo Connection::$param['postgisschema']; ?>/"
                 "wfs_enable_request" "*"
