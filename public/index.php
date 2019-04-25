0090e5d56f0f9e2370f1f892b50e9b1207432079
Martin Høgh <mh@mapcentia.com>
2019-04-25 09:05:14 +0200

    Prepared statements init

--- public/index.php
+++ public/index.php
@@ -131,6 +131,15 @@ if (Input::getPath()->part(1) == "api") {
         Database::setDb($db);
     });
 
+    Route::add("api/v2/preparedstatement/{user}", function () {
+        $db = Route::getParam("user");
+        $dbSplit = explode("@", $db);
+        if (sizeof($dbSplit) == 2) {
+            $db = $dbSplit[1];
+        }
+        Database::setDb($db);
+    });
+
     Route::add("api/v2/qgis/{action}/{user}", function () {
         Database::setDb(Route::getParam("user"));
     });
@@ -151,8 +160,6 @@ if (Input::getPath()->part(1) == "api") {
         Session::start();
         Database::setDb("mapcentia");
     });
-
-
 
     Route::add("api/v1/meta/{user}/[query]", function () {
         Session::start();
