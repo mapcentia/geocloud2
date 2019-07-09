e4548c1041f52bcf6e769c53e05ee8174b8f9086
Martin Høgh <mh@mapcentia.com>
2019-07-09 21:04:11 +0200

    Added copyright and namespace

--- app/conf/migration/Sql.php
+++ app/conf/migration/Sql.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2019 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+namespace app\conf\migration;
 
 class Sql
 {
