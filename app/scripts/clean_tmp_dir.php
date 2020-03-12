7aa4204b610d272d55c74e2b093a62d2e608271c
Martin Høgh <mh@mapcentia.com>
2020-03-12 13:21:25 +0100

    Script for clearing files older that 4h in /app/tmp

---
+++ app/scripts/clean_tmp_dir.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2020 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+include_once(__DIR__ . "/../conf/App.php");
+
+use \app\conf\App;
+
+new App();
+
+$dir = App::$param['path'] . "/app/tmp";
+$ttl = 60 * 60 * 4;
+
+$directory = new \RecursiveDirectoryIterator($dir);
+$iterator = new \RecursiveIteratorIterator($directory);
+$files = array();
+
+foreach ($iterator as $fileInfo) {
+    if ($fileInfo->isFile() && time() - $fileInfo->getCTime() >= $ttl) {
+        echo $fileInfo->getRealPath() . "\n";
+        unlink($fileInfo->getRealPath());
+    }
+}
