25d0b62b9bee53d0e06abb73ee7f21fb25e637e9
Martin Høgh <mh@mapcentia.com>
2020-09-14 12:32:13 +0200

    Check is an empty value is numeric, so 0 becomes a non-empty value.

--- app/wfs/server.php
+++ app/wfs/server.php
@@ -1182,7 +1182,7 @@ function doParse($arr)
                 if ($forSql['fields'][$i][$key] != "gc2_version_uuid" && $forSql['fields'][$i][$key] != "gc2_version_start_date" && $forSql['fields'][$i][$key] != "gc2_version_gid") {
                     if (is_array($value)) {
                         $values[] = "public.ST_Transform(public.ST_GeometryFromText('" . current($value) . "'," . next($value) . ")," . $postgisObject->getGeometryColumns($postgisschema . "." . $forSql['tables'][$i], "srid") . ")";
-                    } elseif (empty($value)) {
+                    } elseif (empty($value) && !is_numeric($value)) {
                         $values[] = "NULL";
                     } elseif ($forSql['fields'][$i][$key] == "gc2_workflow") { // Don't quote a hstore
                         $values[] = $value;
@@ -1291,7 +1291,7 @@ function doParse($arr)
                     }
                 } elseif ($field == "gc2_version_start_date") {
                     $value = "now()";
-                } elseif (empty($forSql2['values'][$i][$key])) {
+                } elseif (empty($forSql2['values'][$i][$key]) && !is_numeric($forSql2['values'][$i][$key])) {
                     $value = "NULL";
                 } else {
                     $value = $postgisObject->quote($forSql2['values'][$i][$key]); // We need to escape the string
