15ace55f2f1c2c184f77e053011dede4eed6b2a4
Martin Høgh <mh@mapcentia.com>
2021-01-04 13:30:03 +0100

    Remove git cmds

--- Gruntfile.js
+++ Gruntfile.js
@@ -295,7 +295,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.registerTask('default', ['npm-install', 'less', 'cssmin', 'jshint', 'hogan', 'preprocess:debug', 'cacheBust']);
-    grunt.registerTask('production', ['gitreset', 'gitpull', 'npm-install', 'less', 'cssmin', 'hogan', 'uglify', 'processhtml', 'preprocess:production', 'cacheBust',  'shell:chown', 'shell:composer']);
+    grunt.registerTask('production', ['npm-install', 'less', 'cssmin', 'hogan', 'uglify', 'processhtml', 'preprocess:production', 'cacheBust',  'shell:chown', 'shell:composer']);
     grunt.registerTask('migration', ['shell:migration']);
 };
 
