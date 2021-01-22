c75c8b1341bccb480cbe3d4c3ed7d76b6d9b8063
Martin Høgh <mh@mapcentia.com>
2021-01-22 13:28:22 +0100

    Update year

--- public/html_header.php
+++ public/html_header.php
@@ -22,7 +22,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 
   ~ @author     Martin Høgh <mh@mapcentia.com>
-  ~ @copyright  2013-2019 MapCentia ApS
+  ~ @copyright  2013-2021 MapCentia ApS
   ~ @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
   ~ @version    2020.12.1
 
