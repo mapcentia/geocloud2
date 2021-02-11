18238b4676a8be5c430b1d4e31d8356f95f86467
Martin Høgh <mh@mapcentia.com>
2021-02-11 09:52:52 +0100

    Clean up

--- app/scripts/clean_tmp_dir.php
+++ app/scripts/clean_tmp_dir.php
@@ -1,11 +1,10 @@
 <?php
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2020 MapCentia ApS
+ * @copyright  2013-2021 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
-
 
 include_once(__DIR__ . "/../conf/App.php");
 
@@ -16,12 +15,11 @@ new App();
 $dir = App::$param['path'] . "/app/tmp";
 $ttl = 60 * 60 * 4;
 
-$directory = new \RecursiveDirectoryIterator($dir);
-$iterator = new \RecursiveIteratorIterator($directory);
-$files = array();
+$directory = new RecursiveDirectoryIterator($dir);
+$iterator = new RecursiveIteratorIterator($directory);
 
 foreach ($iterator as $fileInfo) {
-    if ($fileInfo->isFile() && time() - $fileInfo->getCTime() >= $ttl && $fileInfo->getFilename() !=  ".gitignore") {
+    if ($fileInfo->isFile() && time() - $fileInfo->getCTime() >= $ttl && $fileInfo->getFilename() != ".gitignore") {
         echo $fileInfo->getRealPath() . "\n";
         unlink($fileInfo->getRealPath());
     }
