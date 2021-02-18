72e205a802080c08bd25b6ca86b7d4e6169a0135
Martin Høgh <mh@mapcentia.com>
2021-02-18 13:33:58 +0100

    enableSchedulerCrontab is removed and scheduler.php will only run jobs with db set in App::$params["gc2scheduler"]

--- app/scripts/scheduler.php
+++ app/scripts/scheduler.php
@@ -14,13 +14,8 @@ use app\models\Database;
 use app\inc\Model;
 use GO\Scheduler;
 
+
 new App();
-
-if (empty(App::$param["enableSchedulerCrontab"]) || App::$param["enableSchedulerCrontab"] == false) {
-    echo "Scheduler is disabled\n";
-    exit(0);
-}
-
 Database::setDb("gc2scheduler");
 $scheduler = new Scheduler([
     'tempDir' => '/var/www/geocloud2/app/tmp'
@@ -39,29 +34,31 @@ try {
 }
 
 while ($row = $model->fetchRow($res)) {
-    $args = [
-        "--db" => $row["db"],
-        "--schema" => $row["schema"],
-        "--safeName" => $row["name"],
-        "--url" => urldecode($row["url"]),
-        "--srid" => $row["epsg"],
-        "--type" => $row["type"],
-        "--encoding" => $row["encoding"],
-        "--jobId" => $row["id"],
-        "--deleteAppend" => $row["delete_append"],
-        "--extra" => (base64_encode($row["extra"]) ?: "null"),
-        "--preSql" => (base64_encode($row["presql"]) ?: "null"),
-        "--postSql" => (base64_encode($row["postsql"]) ?: "null"),
-        "--downloadSchema" => $row["download_schema"]
-    ];
-    $cmd = "/var/www/geocloud2/app/scripts/get.php";
-    $scheduler->php(
-        $cmd,
-        "/usr/bin/php",
-        $args,
-        $row["name"]
-    )->at("{$row["min"]} {$row["hour"]} {$row["dayofmonth"]} {$row["month"]} {$row["dayofweek"]}")->output([
-       __DIR__ . "/../../public/logs/{$row["id"]}_scheduler.log"
-    ])->onlyOne();
-    $scheduler->run();
+    if (isset(App::$param["gc2scheduler"][$row["db"]]) && App::$param["gc2scheduler"][$row["db"]] === true) {
+        $args = [
+            "--db" => $row["db"],
+            "--schema" => $row["schema"],
+            "--safeName" => $row["name"],
+            "--url" => urldecode($row["url"]),
+            "--srid" => $row["epsg"],
+            "--type" => $row["type"],
+            "--encoding" => $row["encoding"],
+            "--jobId" => $row["id"],
+            "--deleteAppend" => $row["delete_append"],
+            "--extra" => (base64_encode($row["extra"]) ?: "null"),
+            "--preSql" => (base64_encode($row["presql"]) ?: "null"),
+            "--postSql" => (base64_encode($row["postsql"]) ?: "null"),
+            "--downloadSchema" => $row["download_schema"]
+        ];
+        $cmd = "/var/www/geocloud2/app/scripts/get.php";
+        $scheduler->php(
+            $cmd,
+            "/usr/bin/php",
+            $args,
+            $row["name"]
+        )->at("{$row["min"]} {$row["hour"]} {$row["dayofmonth"]} {$row["month"]} {$row["dayofweek"]}")->output([
+            __DIR__ . "/../../public/logs/{$row["id"]}_scheduler.log"
+        ])->onlyOne();
+        $scheduler->run();
+    }
 }
