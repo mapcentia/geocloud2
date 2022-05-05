f0f31624fb9cf6e900af71e96b149b163f7f6187
Martin Høgh <mh@mapcentia.com>
2022-05-05 14:06:31 +0200

    Add field for symbols

--- app/migration/Sql.php
+++ app/migration/Sql.php
@@ -138,6 +138,7 @@ class Sql
                     file                 varchar,  
                     the_geom             geometry(POINT, 4326)
                 )";
+        $sqls[] = "ALTER TABLE settings.symbols ADD COLUMN timestamp TIMESTAMP WITH TIME ZONE DEFAULT ('now'::TEXT)::TIMESTAMP(0) WITH TIME ZONE";
 
         $sqls[] = "DROP VIEW non_postgis_matviews CASCADE";
         $sqls[] = "CREATE VIEW non_postgis_matviews AS
