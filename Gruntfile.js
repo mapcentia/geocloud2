63cf10eaf4a33ed0c36d99a2d55ea50794ca8ff6
Martin Høgh <mh@mapcentia.com>
2022-10-07 11:30:20 +0200

    CacheBust

--- Gruntfile.js
+++ Gruntfile.js
@@ -208,7 +208,7 @@ module.exports = function (grunt) {
         cacheBust: {
             taskName: {
                 options: {
-                    assets: ['js/**', 'css/**'],
+                    assets: ['js/admin/build/*', 'api/v1/js/*', 'api/v3/js/*', 'css/build/*'],
                     encoding: 'utf8',
                     algorithm: 'md5',
                     length: 16,
