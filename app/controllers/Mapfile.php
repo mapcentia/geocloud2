adbcad7a15efe3e568645aa9f3c09c851aa17f0c
Martin Høgh <mh@mapcentia.com>
2023-11-24 08:52:21 +0100

    Escape bug

--- app/controllers/Mapfile.php
+++ app/controllers/Mapfile.php
@@ -472,7 +472,7 @@ class Mapfile extends Controller
                         } else {
                             $dataSql = $row['data'];
                         }
-                        echo "DATA \"" . strtolower($row['f_geometry_column']) . " FROM (SELECT * FROM ($dataSql) as \"$layerName\" /*FILTER_$layerName*/) as foo USING UNIQUE {$primeryKey['attname']} USING srid={$row['srid']}\"\n";
+                        echo "DATA \"" . strtolower($row['f_geometry_column']) . " FROM (SELECT * FROM ($dataSql) as \\\"$layerName\\\" /*FILTER_$layerName*/) as foo USING UNIQUE {$primeryKey['attname']} USING srid={$row['srid']}\"\n";
                         ?>
                         CONNECTIONTYPE POSTGIS
                         CONNECTION "user=<?php echo Connection::$param['postgisuser']; ?> dbname=<?php echo Connection::$param['postgisdb']; ?><?php if (Connection::$param['postgishost']) echo " host=" . (!empty(Connection::$param['mapserverhost']) ? Connection::$param['mapserverhost'] : Connection::$param['postgishost']); ?><?php echo " port=" . ((!empty(Connection::$param['mapserverport']) ? Connection::$param['mapserverport'] : Connection::$param['postgisport']) ?: "5432") ?><?php if (Connection::$param['postgispw']) echo " password=" . Connection::$param['postgispw']; ?><?php if (!Connection::$param['pgbouncer']) echo " options='-c client_encoding=UTF8'" ?>"
@@ -1472,7 +1472,7 @@ class Mapfile extends Controller
                 } else {
                     $dataSql = $row['data'];
                 }
-                echo "DATA \"" . strtolower($row['f_geometry_column']) . " FROM (SELECT * FROM ($dataSql) as \"$layerName\" /*FILTER_$layerName*/) as foo USING UNIQUE {$primeryKey['attname']} USING srid={$row['srid']}\"\n";
+                echo "DATA \"" . strtolower($row['f_geometry_column']) . " FROM (SELECT * FROM ($dataSql) as \\\"$layerName\\\" /*FILTER_$layerName*/) as foo USING UNIQUE {$primeryKey['attname']} USING srid={$row['srid']}\"\n";
                 ?>
                 CONNECTIONTYPE POSTGIS
                 CONNECTION "user=<?php echo Connection::$param['postgisuser']; ?> dbname=<?php echo Connection::$param['postgisdb']; ?><?php if (Connection::$param['postgishost']) echo " host=" . (!empty(Connection::$param['mapserverhost']) ? Connection::$param['mapserverhost'] : Connection::$param['postgishost']); ?><?php echo " port=" . ((!empty(Connection::$param['mapserverport']) ? Connection::$param['mapserverport'] : Connection::$param['postgisport']) ?: "5432") ?><?php if (Connection::$param['postgispw']) echo " password=" . Connection::$param['postgispw']; ?><?php if (!Connection::$param['pgbouncer']) echo " options='-c client_encoding=UTF8'" ?>"
