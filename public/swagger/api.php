22161e5957d4cc6308d35a621d1732d92140bd40
Martin Høgh <mh@mapcentia.com>
2024-04-03 12:52:31 +0200

    Added header

--- public/swagger/api.php
+++ public/swagger/api.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2024 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
 require("../../app/vendor/autoload.php");
 require("../../app/conf/App.php");
 
