2808ec4d073a87301d2923a7ad964dc3735d4503
Martin Høgh <mh@mapcentia.com>
2024-04-03 09:15:51 +0200

    Remove check-fpm.sh script and corresponding references
    check-fpm.sh bash script has been removed, and all references to it in Dockerfile and supervisord.conf files have been adjusted accordingly. This action simplifies the configuration and decreases potential points of failure. HEALTHCHECK is added in Dockerfile

--- docker/mapserver/check-fpm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true; do
-    	sleep 30;
-	if curl --max-time 25 --output /dev/null --silent --head --fail "127.0.0.1/fpm-ping"; then
-		printf '%s\n' "$url exist"
-	else
-  		/usr/bin/supervisorctl -c /etc/supervisor/conf.d/supervisord.conf restart php-fpm
-	fi
-done
