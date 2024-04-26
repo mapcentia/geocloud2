d663b94f1589c9ceea9f3235af397767145bfcbb
Martin Høgh <mh@mapcentia.com>
2024-04-26 12:52:50 +0200

    Update

--- docker/stable/conf/gc2/App.php
+++ docker/stable/conf/gc2/App.php
@@ -12,14 +12,32 @@ class App
      * @var array<mixed>
      */
     static $param = array(
+
+        "memoryLimit" => "512M",
+
         //Server path where GeoCLoud is installed.
         "path" => "/var/www/geocloud2/",
 
         // When creating new databases use this db as template
         "databaseTemplate" => "template_geocloud",
 
-        // The host of Elasticsearch
-        //"esHost" => "elasticsearch",
+        "SqlApiSettings" => [
+            "statement_timeout" => 120
+        ],
+
+        "sessionHandler" => [
+            "type" => "redis",
+            "host" => "redis:6379", // without tcp:
+            "db" => 1,
+        ],
+
+        // Internal cache system
+        "appCache" => [
+            "type" => "redis",
+            "host" => "redis:6379", // without tcp:
+            "ttl" => 3600,
+            "db" => 0,
+        ],
 
         // MapCache config
         // In Docker use the names of the containers
@@ -34,9 +52,6 @@ class App
             "type" => "disk",
         ],
 
-        // Is MapServer >= 7 used?
-        "mapserver_ver_7" => true,
-
         // Encrypt api key in database
         "encryptSettings" => false,
 
@@ -48,16 +63,7 @@ class App
 
         // Available baselayer
         "baseLayers" => array(
-            array("id" => "stamenToner", "name" => "Stamen Toner"),
             array("id" => "osm", "name" => "OSM"),
-//            array("id" => "mapQuestOSM", "name" => "MapQuset OSM"),
-//            array("id" => "googleStreets", "name" => "Google Street"),
-//            array("id" => "googleHybrid", "name" => "Google Hybrid"),
-//            array("id" => "googleSatellite", "name" => "Google Satellite"),
-//            array("id" => "googleTerrain", "name" => "Google Terrain"),
-//            array("id" => "bingRoad", "name" => "Bing Road"),
-//            array("id" => "bingAerial", "name" => "Bing Aerial"),
-//            array("id" => "bingAerialWithLabels", "name" => "Bing Aerial With Labels"),
         ),
 
         // Bing key
@@ -75,28 +81,16 @@ class App
         // Logo
         "loginLogo" => "/assets/images/MapCentia_500.png",
 
-        // Homepage link for corner logo
-        "homepage" => "http://www.mapcentia.com/en/geocloud/geocloud.htm",
-
         // If true, low layer sort id puts the layer on top
         "reverseLayerOrder" => false,
 
-        // OSM server
-        //"osmConfig" => array(
-        //    "server" => "hostaddr=127.0.0.1 port=5432 dbname=osm user=postgres password=1234",
-        //    "schemas" => array("DK"),
-        //),
-
         // Trust these IPs
         "trustedAddresses" => array(
-          "127.0.0.1/32"
+        //  "127.0.0.1/32"
         ),
 
         // Enable Elasticsearch indexing in GUI
         "esIndexingInGui" => true,
-
-        //Show download options in Heron-mc
-        "showDownloadOtionsInHeron" => true,
 
         //Show workflow options
         "enableWorkflow" => array(
@@ -108,261 +102,11 @@ class App
             "test" => true,
         ),
 
-        // Use API key for Elasticsearch Search API
-        "useKeyForSearch" => false,
-
         // Allowed origins for CORS
         "AccessControlAllowOrigin" => [
             "*"
         ],
 
-        "metaConfig" => [
-            [
-                "fieldsetName" => "Konflikt",
-                "fields" => [
-                    [
-                        "name" => "short_conflict_meta_desc",
-                        "type" => "text",
-                        "title" => "Kort beskrivelse",
-                    ],
-                    [
-                        "name" => "long_conflict_meta_desc",
-                        "type" => "textarea",
-                        "title" => "Lang beskrivelse",
-                    ],
-                ]
-            ],
-            [
-                "fieldsetName" => "Info pop-up",
-                "fields" => [
-                    [
-                        "name" => "info_template",
-                        "type" => "textarea",
-                        "title" => "Pop-up template",
-                    ],
-                    [
-                        "name" => "info_element_selector",
-                        "type" => "text",
-                        "title" => "Element selector",
-                    ],
-                    [
-                        "name" => "info_function",
-                        "type" => "textarea",
-                        "title" => "Function",
-                    ],
-                    [
-                        "name" => "select_function",
-                        "type" => "textarea",
-                        "title" => "Select function",
-                    ],
-                    [
-                        "name" => "accordion_summery_prefix",
-                        "type" => "text",
-                        "title" => "Accordion summery prefix",
-                    ],
-                    [
-                        "name" => "accordion_summery",
-                        "type" => "text",
-                        "title" => "Accordion summery",
-                    ],
-
-                ]
-
-            ],
-            [
-                "fieldsetName" => "Layer type",
-                "fields" => [
-                    [
-                        "name" => "vidi_layer_type",
-                        "type" => "checkboxgroup",
-                        "title" => "Type",
-                        "values" => [
-                            ["name" => "Tile", "value" => "t"],
-                            ["name" => "Vector", "value" => "v"],
-                            ["name" => "WebGL", "value" => "w"],
-                            ["name" => "MVT", "value" => "mvt"],
-                        ],
-                        "default" => "t",
-                    ],
-                    [
-                        "name" => "default_layer_type",
-                        "type" => "combo",
-                        "title" => "Default",
-                        "values" => [
-                            ["name" => "Tile", "value" => "t"],
-                            ["name" => "Vector", "value" => "v"],
-                            ["name" => "WebGL", "value" => "w"],
-                            ["name" => "MVT", "value" => "mvt"],
-                        ],
-                        "default" => "t",
-                    ],
-                ]
-
-            ],
-            [
-                "fieldsetName" => "Tables",
-                "fields" => [
-                    [
-                        "name" => "zoom_on_table_click",
-                        "type" => "checkbox",
-                        "title" => "Zoom on select",
-                        "default" => false,
-                    ],
-                ]
-            ],
-            [
-                "fieldsetName" => "Editor",
-                "fields" => [
-                    [
-                        "name" => "vidi_layer_editable",
-                        "type" => "checkbox",
-                        "title" => "Editable",
-                        "default" => false,
-                    ],
-                ]
-
-            ],
-            [
-                "fieldsetName" => "Tile settings",
-                "fields" => [
-                    [
-                        "name" => "single_tile",
-                        "type" => "checkbox",
-                        "title" => "Use tile cache",
-                        "default" => false,
-                    ],
-                    [
-                        "name" => "tiles_service_uri",
-                        "type" => "text",
-                        "title" => "Tiles service uri",
-                    ],
-                    [
-                        "name" => "tiles_selected_style",
-                        "type" => "textarea",
-                        "title" => "Selected style",
-                    ],
-                ]
-
-            ],
-            [
-                "fieldsetName" => "Vector settings",
-                "fields" => [
-                    [
-                        "name" => "load_strategy",
-                        "type" => "combo",
-                        "title" => "Load strategy",
-                        "values" => [
-                            ["name" => "Static", "value" => "s"],
-                            ["name" => "Dynamic", "value" => "d"],
-                        ],
-                        "default" => "s",
-                    ],
-                    [
-                        "name" => "max_features",
-                        "type" => "text",
-                        "title" => "Max features",
-                        "default" => "500",
-                    ],
-                    [
-                        "name" => "use_clustering",
-                        "type" => "checkbox",
-                        "title" => "Use clustering",
-                        "default" => false,
-                    ],
-                    [
-                        "name" => "point_to_layer",
-                        "type" => "textarea",
-                        "title" => "Point to layer",
-                    ],
-                    [
-                        "name" => "vector_style",
-                        "type" => "textarea",
-                        "title" => "Style function",
-                    ],
-                    [
-                        "name" => "show_table_on_side",
-                        "type" => "checkbox",
-                        "title" => "Show table",
-                        "default" => false,
-                    ],
-                    [
-                        "name" => "reload_interval",
-                        "type" => "text",
-                        "title" => "Reload Interval",
-                    ],
-                    [
-                        "name" => "disable_vector_feature_info",
-                        "type" => "checkbox",
-                        "title" => "Disable feature info",
-                        "default" => false,
-                    ],
-                ]
-
-            ],
-            [
-                "fieldsetName" => "Filters",
-                "fields" => [
-                    [
-                        "name" => "filter_config",
-                        "type" => "textarea",
-                        "title" => "Filter config",
-                    ],
-                    [
-                        "name" => "predefined_filters",
-                        "type" => "textarea",
-                        "title" => "Predefined filters",
-                    ],
-                    [
-                        "name" => "default_match",
-                        "type" => "combo",
-                        "title" => "Default match",
-                        "values" => [
-                            ["name" => "All", "value" => "all"],
-                            ["name" => "Any", "value" => "any"],
-                        ],
-                        "default" => "any",
-                    ],
-                    [
-                        "name" => "filter_immutable",
-                        "type" => "checkbox",
-                        "title" => "Immutable",
-                        "default" => false,
-                    ],
-                ]
-
-            ],
-            [
-                "fieldsetName" => "References",
-                "fields" => [
-                    [
-                        "name" => "referenced_by",
-                        "type" => "textarea",
-                        "title" => "Referenced by",
-                    ],
-                ]
-            ],
-            [
-                "fieldsetName" => "Layer tree",
-                "fields" => [
-                    [
-                        "name" => "vidi_sub_group",
-                        "type" => "text",
-                        "title" => "Sub group",
-                    ],
-                    [
-                        "name" => "default_open_tools",
-                        "type" => "textarea",
-                        "title" => "Open tools",
-                    ],
-                    [
-                        "name" => "disable_check_box",
-                        "type" => "checkbox",
-                        "title" => "Disable check box",
-                        "default" => false,
-                    ],
-                ],
-            ],
-        ],
         // Meta properties
         "vidiUrl" => "http://127.0.0.1:3000",
 
