b0564a605080e5ce3491395334d6a54b726a4a1c
Martin Høgh <mh@mapcentia.com>
2024-05-07 15:10:18 +0200

    Add author information and extra Throwable import to GC2Exception
    The commit introduces a docblock to the GC2Exception.php file, containing author information, copyright, and license details. Additionally, it imports the Throwable class and adds it as a parameter to the GC2Exception class constructor.

--- app/exceptions/GC2Exception.php
+++ app/exceptions/GC2Exception.php
@@ -1,12 +1,21 @@
 <?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2024 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
 
 namespace app\exceptions;
 
 use Exception;
+use Throwable;
 
 class GC2Exception extends Exception
 {
     protected string|null $errorCode;
+
     public function __construct($message, $code = 0, Throwable $previous = null, string $errorCode = null) {
         $this->errorCode = $errorCode;
         parent::__construct($message, $code, $previous);
