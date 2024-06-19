a325e46296c434799c39852f52be065c4d1cde2c
Martin Høgh <mh@mapcentia.com>
2024-06-19 13:46:48 +0200

    Update job reports and error notifications
    Updated the job reports script to include only active jobs in its scope, and improved error notification by grouping job errors per database. Removed the unnecessary import of PostmarkException to eliminate code clutter. Also, modified the copyright year.

--- app/scripts/job_report.php
+++ app/scripts/job_report.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2021 MapCentia ApS
+ * @copyright  2013-2024 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
@@ -13,7 +13,6 @@ use app\conf\App;
 use app\inc\Model;
 use app\models\Database;
 use Postmark\PostmarkClient;
-use Postmark\Models\PostmarkException;
 
 new App();
 
@@ -24,7 +23,7 @@ $model = new Model();
 
 $failedJobs = [];
 
-$sql = "SELECT * FROM jobs WHERE lastcheck = FALSE AND lastrun > (now() - '24 hour' :: INTERVAL) ORDER BY id";
+$sql = "SELECT * FROM jobs WHERE lastcheck = FALSE AND active = TRUE AND lastrun > (now() - '24 hour' :: INTERVAL) ORDER BY id";
 
 $res = $model->prepare($sql);
 try {
@@ -33,37 +32,43 @@ try {
     print_r($e);
     exit(1);
 }
-$failedJobs[] = "<table class=\"table table-striped\">\n";
-$failedJobs[] = "<tr><th>Id</th><th>Database</th><th>Schema</th><th>Table</th><th>Log</th></tr>\n";
+$header[] = "<table class=\"table table-striped\">\n";
+$header[] = "<tr><th>Id</th><th>Database</th><th>Schema</th><th>Table</th><th>Log</th></tr>\n";
+$rows = [];
 while ($row = $model->fetchRow($res)) {
-    $failedJobs[] = "<tr><td>{$row["id"]}</td><td>{$row["db"]}</td><td>{$row["schema"]}</td><td>{$row["name"]}</td><td><a target=\"_blank\" href=\"" . App::$param["notification"]["logUrl"] . "/logs/" . $row["id"] . "_scheduler.log\">{$row["id"]}_scheduler.log</a></td></tr>\n";
+    $rows[$row["db"]][] = $row;
 }
-$failedJobs[] = "</table>\n";
+$databases = [];
+foreach ($rows as $db => $jobs) {
+    $databases[] = $db;
+}
+$footer = "</table>\n";
 
-$html = "<p>Failed jobs in the last 24 hours</p>\n";
-$html .= implode("", $failedJobs);
-
-if (App::$param["notification"]) {
-    try {
-        $client = new PostmarkClient(App::$param["notification"]["key"]);
-        $message = [
-            'To' => implode(",", App::$param["notification"]["to"]),
-            'From' => App::$param["notification"]["from"],
-            'TrackOpens' => false,
-            'Subject' => "GC2Scheduler jobs with error. " . App::$param["notification"]["logUrl"],
-            'HtmlBody' => "<html><head><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\"/></head><body style=\"padding: 20px\">" . $html . "</body></html>",
-        ];
+foreach ($databases as $db) {
+    if (App::$param["notification"]["to"][$db]) {
+        $tr = [];
+        foreach ($rows[$db] as $row) {
+            $tr[] = "<tr><td>{$row["id"]}</td><td>{$row["db"]}</td><td>{$row["schema"]}</td><td>{$row["name"]}</td><td><a target=\"_blank\" href=\"" . App::$param["notification"]["logUrl"] . "/logs/" . $row["id"] . "_scheduler.log\">{$row["id"]}_scheduler.log</a></td></tr>\n";
+        }
+        $html = "<p>Failed jobs in the last 24 hours for database $db</p>\n";
         try {
-            $sendResult = $client->sendEmailBatch([$message]);
-            print_r($sendResult);
+            $client = new PostmarkClient(App::$param["notification"]["key"]);
+            $message = [
+                'To' => implode(",", App::$param["notification"]["to"][$db]),
+                'From' => App::$param["notification"]["from"],
+                'TrackOpens' => false,
+                'Subject' => "Errors in scheduler jobs for database $db",
+                'HtmlBody' => "<html lang='en'><head><title>Errors in scheduler jobs</title><link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\"/></head><body style=\"padding: 20px\">" . $html . implode('', $header) . implode('', $tr) . $footer . "</body></html>",
+            ];
+            try {
+                $sendResult = $client->sendEmailBatch([$message]);
+                print_r($sendResult);
+            } catch (Exception $generalException) {
+                exit(1);
+            }
         } catch (Exception $generalException) {
             exit(1);
         }
-    } catch (PostmarkException $ex) {
-        echo $ex->httpStatusCode;
-        echo $ex->postmarkApiErrorCode;
-    } catch (Exception $generalException) {
-        exit(1);
     }
 }
 exit(0);
