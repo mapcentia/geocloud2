ad7260d6c1379560c6c6f4a2aba9528d1e3b4414
Martin Høgh <mh@mapcentia.com>
2024-08-14 13:53:41 +0200

    Enhance user experience and fix device code handling
    Added autofocus attribute to improve user experience in various forms. Improved device code handling by integrating Jwt class and providing explicit user feedback through alerts.

--- app/auth/backends/device.php
+++ app/auth/backends/device.php
@@ -2,6 +2,7 @@
 
 use app\exceptions\GC2Exception;
 use app\inc\Cache;
+use app\inc\Jwt;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
@@ -13,15 +14,15 @@ $CachedString = Cache::getItem($code);
 
 if ($CachedString != null && $CachedString->isHit()) {
     $val = $CachedString->get();
-    echo $val;
-    print_r($_SESSION);
     if (!empty($val) && $val == 1) {
-        $CachedString->set($_SESSION)->expiresAfter(\app\inc\Jwt::DEVICE_CODE_TTL);
+        $CachedString->set($_SESSION)->expiresAfter(Jwt::DEVICE_CODE_TTL);
         Cache::save($CachedString);
+        echo "<div id='alert' hx-swap-oob='true'>Device code founded</div>";
     } else {
+        echo $twig->render("device.html.twig");
+        echo "<div id='alert' hx-swap-oob='true'>Device code already used</div>";
     }
 } else {
-
     echo $twig->render("device.html.twig");
     echo "<div id='alert' hx-swap-oob='true'>Code doesn't exists</div>";
 }
