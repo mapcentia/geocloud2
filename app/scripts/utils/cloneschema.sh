9be169d04b2fc1eddfbb84dbdd2cfc9ec277b77d
Martin Høgh <mh@mapcentia.com>
2024-09-06 09:31:31 +0200

    Remove hardcoded host and credentials from cloneschema.sh
    The script no longer hardcodes the host and database credentials, making it more flexible and secure. This update ensures the script will use system defaults or environment variables, allowing for easier configuration and deployment in different environments.

--- app/scripts/utils/cloneschema.sh
+++ app/scripts/utils/cloneschema.sh
@@ -5,8 +5,6 @@
 OPTIND=1
 
 # Initialize our own variables:
-HOST="127.0.0.1"
-CREDS="--user=postgres"
 DUMPPATH="clone_schema_tmp.sql"
 TMPDB="cloneschema_tmp"
 TMPCOPY="geometry_columns_join_tmp.sql"
@@ -45,27 +43,27 @@ fi
 echo "from=$from, to=$to, db=$db"
 
 # Dump the original DB
-pg_dump --host ${HOST} ${CREDS} --format=c ${db} > ${DUMPPATH}
+pg_dump --format=c ${db} > ${DUMPPATH}
 
 # Create the tmp DB and restore the dump in it
-createdb --host ${HOST} ${CREDS} ${TMPDB} --template=template0 --locale=da_DK.UTF-8 --encoding=UTF8
-#psql --host ${HOST} ${CREDS} ${TMPDB} -c "CREATE EXTENSION postgis"
-pg_restore --host ${HOST} ${CREDS} --dbname ${TMPDB} ${DUMPPATH}
+createdb ${TMPDB} --template=template0 --locale=da_DK.UTF-8 --encoding=UTF8
+#psql ${TMPDB} -c "CREATE EXTENSION postgis"
+pg_restore --dbname ${TMPDB} ${DUMPPATH}
 
 # Rename the schema in the tmp DB
-psql --host ${HOST} ${CREDS} --dbname ${TMPDB} -c "ALTER SCHEMA ${from} RENAME TO ${to}"
+psql --dbname ${TMPDB} -c "ALTER SCHEMA ${from} RENAME TO ${to}"
 
 # Dump the tmp DB and restore the renamed schema in the original
-pg_dump --host ${HOST} ${CREDS} --format=c  ${TMPDB} > ${DUMPPATH}
-psql --host ${HOST} ${CREDS} --dbname ${db} -c "CREATE SCHEMA ${to}"
-pg_restore --host ${HOST} ${CREDS} --dbname ${db} --schema ${to} ${DUMPPATH}
+pg_dump --format=c  ${TMPDB} > ${DUMPPATH}
+psql --dbname ${db} -c "CREATE SCHEMA ${to}"
+pg_restore --dbname ${db} --schema ${to} ${DUMPPATH}
 
 # Update the settings.geometry_columns_join table in the tmp DB
-psql --host ${HOST} ${CREDS} --dbname ${TMPDB} -c "UPDATE  settings.geometry_columns_join set _key_ = '${to}'||'.'||split_part(_key_, '.', 2)||'.'||split_part(_key_, '.', 3) WHERE split_part(_key_, '.', 1) = '${from}'"
+psql --dbname ${TMPDB} -c "UPDATE  settings.geometry_columns_join set _key_ = '${to}'||'.'||split_part(_key_, '.', 2)||'.'||split_part(_key_, '.', 3) WHERE split_part(_key_, '.', 1) = '${from}'"
 
 # Copy the updated rows from settings.geometry_columns_join and restore them in the original DB
-psql --host ${HOST} ${CREDS} ${TMPDB} -c "\COPY (SELECT * FROM settings.geometry_columns_join WHERE split_part(_key_, '.', 1) = '${to}') TO STDOUT" > ${TMPCOPY}
-psql --host ${HOST} ${CREDS} ${db} -c "\COPY settings.geometry_columns_join FROM 'geometry_columns_join_tmp.sql'"
+psql ${TMPDB} -c "\COPY (SELECT * FROM settings.geometry_columns_join WHERE split_part(_key_, '.', 1) = '${to}') TO STDOUT" > ${TMPCOPY}
+psql ${db} -c "\COPY settings.geometry_columns_join FROM 'geometry_columns_join_tmp.sql'"
 
 #Clean up
 dropdb cloneschema_tmp -U postgres
