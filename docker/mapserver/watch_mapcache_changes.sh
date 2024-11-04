f8f5c2b312fdb5e8f1b34ef6536e8bbaa8202721
Martin Høgh <mh@mapcentia.com>
2024-11-04 17:15:20 +0100

    Line endings

--- docker/mapserver/watch_mapcache_changes.sh
+++ docker/mapserver/watch_mapcache_changes.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-listcommand="ls /var/www/geocloud2/app/wms/mapcache/*.xml -l $*"
-
-newfilelist=$( $listcommand 2>/dev/null)
-while true
-do
-	if [[ $oldfilelist != $newfilelist ]]
-	then
-		oldfilelist=$newfilelist
-		/usr/bin/node /reload.js
-	fi
-	sleep 0.1
-	newfilelist=$( $listcommand 2>/dev/null)
-done
-
+#!/bin/bashlistcommand="ls /var/www/geocloud2/app/wms/mapcache/*.xml -l $*"newfilelist=$( $listcommand 2>/dev/null)while truedo	if [[ $oldfilelist != $newfilelist ]]	then		oldfilelist=$newfilelist		/usr/bin/node /reload.js	fi	sleep 0.1	newfilelist=$( $listcommand 2>/dev/null)done
