42ac7003ef2f29676a54b5ef5ae9fe08fa67302a
Martin Høgh <mh@mapcentia.com>
2024-12-02 11:53:18 +0100

    Add unit test skeleton for WfsFilter
    Introduce an initial setup for WfsFilter unit tests with a test placeholder. This prepares the groundwork for testing filter logic in future implementations.

---
+++ app/tests/unit/WfsFilterTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2024 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+use Codeception\Test\Unit;
+
+class WfsFilterTest extends Unit
+{
+    protected UnitTester $tester;
+    public function testNotFilter(): void {
+
+    }
+
+}
