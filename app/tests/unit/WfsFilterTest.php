a986f437357ee5989bb831af68d3e565cb55e8a4
Martin Høgh <mh@mapcentia.com>
2024-12-03 14:00:58 +0100

    Update

--- app/tests/unit/WfsFilterTest.php
+++ app/tests/unit/WfsFilterTest.php
@@ -11,7 +11,17 @@ use Codeception\Test\Unit;
 class WfsFilterTest extends Unit
 {
     protected UnitTester $tester;
+
+    protected function _before(): void
+    {
+    }
     public function testNotFilter(): void {
+        include_once dirname(__DIR__) . '/../wfs/server.php';
+
+        $xml = '<ogc:PropertyIsEqualTo xmlns:ogc="http://www.opengis.net/ogc">
+  <ogc:PropertyName>gid</ogc:PropertyName>
+  <ogc:Literal>1</ogc:Literal>
+</ogc:PropertyIsEqualTo>';
 
     }
 
