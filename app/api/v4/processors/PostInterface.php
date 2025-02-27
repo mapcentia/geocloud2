4ba2f4d26606017ef1c0aa65f171a2a02f6f7c5a
Martin Høgh <mh@mapcentia.com>
2025-02-27 12:45:22 +0100

    Added PostExtension in API v4 with a processGetTable method, which can mutate the table object before returning it

---
+++ app/api/v4/processors/PostInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2024 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+namespace app\api\v4\processors;
+
+
+use app\inc\Model;
+
+interface PostInterface
+{
+    public function processGetTable(Model $model, array $response) : array;
+}
