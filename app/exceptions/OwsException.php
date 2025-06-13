bb8c1a835007cbda03de48fdfaa804b653064a55
Martin Høgh <mh@mapcentia.com>
2025-06-13 09:38:33 +0200

    Update copyright year and enhance OwsException documentation
    Updated the copyright year from 2024 to 2025. Added detailed class-level documentation to describe the purpose, attributes, and functionality of the OwsException class in the context of OGC OWS specifications.

--- app/exceptions/OwsException.php
+++ app/exceptions/OwsException.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2024 MapCentia ApS
+ * @copyright  2013-2025 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
@@ -12,6 +12,15 @@ namespace app\exceptions;
 use Exception;
 use Throwable;
 
+/**
+ * The OwsException class represents a specialized exception used for generating
+ * Exception Reports in the OGC (Open Geospatial Consortium) OWS (OGC Web Services) context.
+ * It extends the base PHP Exception class, allowing for the inclusion of additional attributes
+ * relevant to the exception and formatting the response according to OWS ExceptionReport specifications.
+ *
+ * This exception class provides mechanisms for storing key-value attributes
+ * and generating a structured XML representation compliant with the OWS specification.
+ */
 class OwsException extends Exception
 {
     protected array $attributes;
