85b818193efb854b47ef3c42ac10dc5111cb858e
Martin Høgh <mh@mapcentia.com>
2025-10-03 09:55:45 +0200

    Update `GetPart` class to specify property type for `$parts` and extend copyright year to 2025.

--- app/inc/GetPart.php
+++ app/inc/GetPart.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2021 MapCentia ApS
+ * @copyright  2013-2025 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
@@ -18,7 +18,7 @@ class GetPart
     /**
      * @var array<string>
      */
-    private $parts;
+    private array $parts;
 
     /**
      * GetPart constructor.
