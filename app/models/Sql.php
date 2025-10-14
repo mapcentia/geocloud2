cde4e164d7019d6483f631cf3c6e1d16fe87a4e3
Martin Høgh <mh@mapcentia.com>
2025-10-14 15:51:15 +0200

    Fix `convertDataUrlsToHttp` logic to ensure correct SQL key quoting in generated URLs.

--- app/models/Sql.php
+++ app/models/Sql.php
@@ -214,7 +214,7 @@ class Sql extends Model
                 if (!empty(App::$param['convertDataUrlsToHttp']) && $rel) {
                     // Convert data URLs to HTTP. Read the first bytes to get the mimetype.
                     $rowValue = App::$param['host'] . "/api/v1/decodeimg/" . $this->postgisdb . "/" . $rel . "/" . $key . "/";
-                    $fieldsArr[] = "'$rowValue'||gid||'?mimetype='||SPLIT_PART(SPLIT_PART(encode(substring($key from 0 for 100),'escape'),';',1),':',2) as \"$key\"";
+                    $fieldsArr[] = "'$rowValue'||gid||'?mimetype='||SPLIT_PART(SPLIT_PART(encode(substring(\"$key\" from 0 for 100),'escape'),';',1),':',2) as \"$key\"";
                 } else {
                     $fieldsArr[] = "encode(\"$key\",'escape') as \"$key\"";
                 }
