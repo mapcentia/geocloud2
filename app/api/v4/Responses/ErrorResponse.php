f509c44c49c9d507ec7b9af1df956b359776f443
Martin Høgh <mh@mapcentia.com>
2025-10-15 13:08:20 +0200

    Allow `ErrorResponse` to accept `array|string|null` for `$data` parameter in constructor.

--- app/api/v4/Responses/ErrorResponse.php
+++ app/api/v4/Responses/ErrorResponse.php
@@ -5,7 +5,7 @@ namespace app\api\v4\Responses;
 
 final class ErrorResponse extends Response
 {
-    public function __construct(protected ?array $data, int $status = 400)
+    public function __construct(protected array|string|null $data, int $status = 400)
     {
         parent::__construct($status, $data);
     }
