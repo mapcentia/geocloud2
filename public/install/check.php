14b1b778a0d8eb7562ead1f5c910da87703141d6
Martin Høgh <mh@mapcentia.com>
2026-02-12 11:45:15 +0100

    Add `check.php` security layer to installation scripts and update copyright year to 2026

---
+++ public/install/check.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2026 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+
+use app\inc\Util;
+use app\conf\App;
+
+$trusted = false;
+foreach (App::$param["allowedToInstall"] as $address) {
+    if (Util::ipInRange(Util::clientIp(), $address)) {
+        $trusted = true;
+        break;
+    }
+}
+
+if (!$trusted) {
+    header('HTTP/1.0 404 Not Found');
+    echo "<h1>404 Not Found</h1>\n";
+    echo "<!--"  . Util::clientIp() . "-->";
+    exit;
+}
--- public/install/prepare.php
+++ public/install/prepare.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2025 MapCentia ApS
+ * @copyright  2013-2026 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  *
  */
@@ -29,6 +29,9 @@ include("../../app/models/Setting.php");
 include("../../app/exceptions/GC2Exception.php");
 
 new App();
+
+include("./check.php");
+
 Cache::setInstance();
 
 // sql.php defines $sql for creating the GC2 settings schema
