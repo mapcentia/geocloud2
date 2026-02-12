89feeabae08f01ecd2b4537dbf2f21c734efd397
Martin Høgh <mh@mapcentia.com>
2026-02-12 08:40:45 +0100

    Add author, copyright, and license headers to all response classes in `v4/Responses`.

--- app/api/v4/Responses/Response.php
+++ app/api/v4/Responses/Response.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2026 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
 namespace app\api\v4\Responses;
 
 abstract class Response
