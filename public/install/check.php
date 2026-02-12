775af50ef294ff1d27edf6b77552cfc32fbbc4de
Martin Høgh <mh@mapcentia.com>
2026-02-12 11:49:37 +0100

    Ensure `allowedToInstall` parameter is set before iterating in `check.php` to prevent potential errors.

--- public/install/check.php
+++ public/install/check.php
@@ -11,16 +11,18 @@ use app\inc\Util;
 use app\conf\App;
 
 $trusted = false;
-foreach (App::$param["allowedToInstall"] as $address) {
-    if (Util::ipInRange(Util::clientIp(), $address)) {
-        $trusted = true;
-        break;
+if (isset(App::$param["allowedToInstall"])) {
+    foreach (App::$param["allowedToInstall"] as $address) {
+        if (Util::ipInRange(Util::clientIp(), $address)) {
+            $trusted = true;
+            break;
+        }
     }
-}
 
-if (!$trusted) {
-    header('HTTP/1.0 404 Not Found');
-    echo "<h1>404 Not Found</h1>\n";
-    echo "<!--"  . Util::clientIp() . "-->";
-    exit;
+    if (!$trusted) {
+        header('HTTP/1.0 404 Not Found');
+        echo "<h1>404 Not Found</h1>\n";
+        echo "<!--" . Util::clientIp() . "-->";
+        exit;
+    }
 }
