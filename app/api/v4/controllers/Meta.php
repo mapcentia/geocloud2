958a7a62918c96a83642297aae134620e741c0fe
Martin Høgh <mh@mapcentia.com>
2026-02-25 21:48:10 +0100

    Add PATCH support to Meta API, restructure response handling, and refine field validation logic

--- app/api/v4/controllers/Meta.php
+++ app/api/v4/controllers/Meta.php
@@ -13,27 +13,33 @@ use app\api\v4\AcceptableAccepts;
 use app\api\v4\AcceptableMethods;
 use app\api\v4\Controller;
 use app\api\v4\Responses\GetResponse;
+use app\api\v4\Responses\PatchResponse;
 use app\api\v4\Responses\Response;
 use app\api\v4\Scope;
 use app\exceptions\GC2Exception;
 use app\inc\Connection;
+use app\inc\Input;
 use app\inc\Jwt;
 use app\inc\Route2;
 use app\models\Layer;
 use Override;
 use Phpfastcache\Exceptions\PhpfastcacheInvalidArgumentException;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * Class Meta
  * @package app\api\v4
  */
-#[AcceptableMethods(['GET', 'HEAD', 'OPTIONS'])]
+#[AcceptableMethods(['GET', 'PATCH', 'HEAD', 'OPTIONS'])]
 #[Controller(route: 'api/v3/meta/[query]', scope: Scope::SUB_USER_ALLOWED)]
 class Meta extends AbstractApi
 {
-    /**
-     * Meta constructor.
-     */
+    private const array PRIVATE_PROPERTIES = ['num', 'typname', 'full_type', 'character_maximum_length',
+        'numeric_precision', 'numeric_scale', 'max_bytes', 'reference', 'restriction', 'is_primary', 'is_unique',
+        'index_method', 'checks', 'geom_type', 'srid', 'is_array', 'udt_name', 'identity_generation',
+        'default_value', 'type', 'comment', 'is_nullable'];
+
     public function __construct(public readonly Route2 $route, Connection $connection)
     {
         parent::__construct($connection);
@@ -78,17 +84,39 @@ class Meta extends AbstractApi
     {
         $layers = new Layer(connection: $this->connection);
         $jwt = Jwt::validate()["data"];
-        $res = $layers->getAll($jwt["database"], true, $this->route->getParam("query"), false, true, false, false);
+        $res = $layers->getAll(
+            db: $jwt["database"],
+            auth: true,
+            query: $this->route->getParam("query"),
+            parse: true,
+            lookupForeignTables: false,
+            jwt: $jwt,
+        );
         $rows = $res["data"];
         $r = self::processRows($rows);
         return new GetResponse(data: ['relations' => $r]);
+    }
+
+    public function patch_index(): Response
+    {
+        $body = Input::getBody();
+        $data = json_decode($body, true);
+        $geometryJoinTable = new \app\models\Table(table: "settings.geometry_columns_join", connection: $this->connection);
+        $geometryJoinTable->begin();
+        foreach ($data['relations'] as $key => $datum) {
+            $geometryJoinTable->updateRecord(data: self::processRowReverse($datum), keyName: '_key_');
+
+        }
+        $geometryJoinTable->commit();
+
+        return new PatchResponse(data: []);
     }
 
     static function processRows(array $rows): array
     {
         $out = [];
         foreach ($rows as $row) {
-            $out[$row["_key_"]] = self::processRow($row);
+            $out[] = self::processRow($row);
         }
         return $out;
     }
@@ -96,33 +124,104 @@ class Meta extends AbstractApi
     static function processRow(array $row): array
     {
         return [
-            "uuid" => $row["uuid"],
-            "f_table_schema" => $row["f_table_schema"],
-            "f_table_name" => $row["f_table_name"],
-            "f_geometry_column" => $row["f_geometry_column"],
-            "f_table_abstract" => $row["f_table_abstract"],
-            "f_table_title" => $row["f_table_title"],
-            "pkey" => $row["pkey"],
-            "rel_type" => $row["rel_type"],
-            "coord_dimension" => $row["coord_dimension"],
-            "type" => $row["type"],
-            "srid" => $row["srid"],
-            "authentication" => $row["authentication"],
-            "layergroup" => $row["layergroup"],
+            "title" => $row["f_table_title"],
+            "abstract" => $row["f_table_abstract"],
+            "group" => $row["layergroup"],
             "sort_id" => $row["sort_id"],
-//            "wmssource" => $row["wmssource"],
             "tags" => $row["tags"],
-//            "privileges" => $row["privileges"],
-            "fields" => $row["fields"],
-            "children" => $row["children"],
             "meta" => $row["meta"],
+            "fields" => self::setPropertiesToPrivate($row["fields"]),
+
+            "_uuid" => $row["uuid"],
+            "_key" => $row["_key_"],
+            "_schema" => $row["f_table_schema"],
+            "_rel" => $row["f_table_name"],
+            "_geometry_column" => $row["f_geometry_column"],
+            "_pkey" => $row["pkey"],
+            "_rel_type" => $row["rel_type"],
+            "_coord_dimension" => $row["coord_dimension"],
+            "_geom_type" => $row["type"],
+            "_srid" => $row["srid"],
+            "_authentication" => $row["authentication"],
+//            "wmssource" => $row["wmssource"],
+//            "privileges" => $row["privileges"],
+            "_children" => $row["children"],
         ];
+
+    }
+
+    static function processRowReverse(array $row): array
+    {
+        return [
+            "_key_" => $row["_key"],
+            "f_table_abstract" => $row["abstract"],
+            "f_table_title" => $row["title"],
+            "layergroup" => $row["group"],
+            "sort_id" => $row["sort_id"],
+            "tags" => $row["tags"],
+            "meta" => $row["meta"],
+            "fieldconf" => ($row["fields"]),
+        ];
+
+    }
+
+    protected static function setPropertiesToPrivate(array $properties): array
+    {
+        $newArray = [];
+        foreach ($properties as $field => $property) {
+            $col = [];
+            foreach ($property as $key => $value) {
+                if (in_array($key, self::PRIVATE_PROPERTIES)) {
+                    //$col['_' . $key] = $value;
+                } else {
+                    $col[$key] = $value;
+                }
+            }
+            $newArray[$field] = $col;
+        }
+        return $newArray;
     }
 
     #[Override]
     public function validate(): void
     {
-        // TODO: Implement validate() method.
+        $body = Input::getBody();
+        $this->validateRequest(
+            collection: self::getAssert(),
+            data: $body,
+            method: Input::getMethod(),
+            allowPatchOnCollection: true
+        );
+    }
+
+    private function getAssert(): Assert\Collection
+    {
+        return new Assert\Collection(
+            fields: [
+                'title' => new Assert\Optional(new Assert\Type('string')),
+                'abstract' => new Assert\Optional(new Assert\Type('string')),
+                'group' => new Assert\Optional(new Assert\Type('string')),
+                'sort_id' => new Assert\Optional(new Assert\Type('integer')),
+                'tags' => new Assert\Optional(new Assert\Type('array')),
+                'meta' => new Assert\Optional(new Assert\Type('array', 'This value should be of type object.')),
+                'fields' => new Assert\Optional([
+                    new Assert\Type('array'),
+                    new Assert\All([
+                        new Assert\Collection(
+                            fields: [
+                                'alias' => new Assert\Optional(new Assert\Type('string')),
+                                'queryable' => new Assert\Optional(new Assert\Type('boolean')),
+                                'sort_id' => new Assert\Optional(new Assert\Type('integer')),
+                            ],
+                            allowExtraFields: false,
+                            allowMissingFields: true,
+                        )
+                    ])
+                ]),
+            ],
+            allowExtraFields: true,
+            allowMissingFields: true,
+        );
     }
 
     #[Override]
@@ -142,10 +241,5 @@ class Meta extends AbstractApi
     {
         // TODO: Implement delete_index() method.
 
-    }
-
-    public function patch_index(): Response
-    {
-        // TODO: Implement patch_index() method.
     }
 }
