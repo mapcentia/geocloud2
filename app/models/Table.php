8e26526ea5f23c2e4b88c1bbdfeefdad4927101b
Martin Høgh <mh@mapcentia.com>
2026-05-08 14:19:53 +0200

    Add `enabledPseudoNotNull` check to handle nullable column updates and schema parsing

--- app/models/Table.php
+++ app/models/Table.php
@@ -789,7 +789,8 @@ class Table extends Model
             }
 
             // Set nullable
-            if ($this->metaData[$value->id]["is_nullable"] != $value->is_nullable && !$onlyRename) {
+            $enabledPseudoNotNull = App::$param['enabledPseudoNotNull'] ?? false;
+            if (!$enabledPseudoNotNull && $this->metaData[$value->id]["is_nullable"] != $value->is_nullable && !$onlyRename) {
                 $sql = "ALTER TABLE " . $this->doubleQuoteQualifiedName($this->table) . " ALTER \"$value->column\" " . ($value->is_nullable ? "DROP" : "SET") . " NOT NULL";
                 $res = $this->prepare($sql);
                 try {
